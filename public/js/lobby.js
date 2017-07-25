5fe2c7cf8da2c5c96bb9dbb145bb876cd010c9cf
James Nesta <j.nesta@gmail.com>
2017-07-25 08:37:22 +0000

    fixing bug where variants were not being filled in

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -454,10 +454,12 @@ var variant_names = [
     "Mixed-color Suits",
     "Mixed and Multi-color Suits"
 ];
-for (let i = 0; i < variant_names.length; i++) {
-    var option = new Option(variant_names[i], i);
-    $("#create-game-variant").append($(option));
-}
+$(document).ready(() => {
+    for (let i = 0; i < variant_names.length; i++) {
+        var option = new Option(variant_names[i], i);
+        $("#create-game-variant").append($(option));
+    }
+});
 
 HanabiLobby.prototype.draw_tables = function() {
     var self = this;
