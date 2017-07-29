5e03dd972bbde950ac406b18f69226454ba3590f
James Nesta <j.nesta@gmail.com>
2017-07-29 22:15:07 +0000

    better debugging msg

--- debug.js
+++ debug.js
@@ -3,20 +3,25 @@ const globals = require('./globals');
 const logger = require('./logger');
 
 exports.step1 = (socket, data) => {
-    // Local variables
-    let i;
-
     logger.info('globals.connectedUsers:');
-    i = 1;
-    for (const userID of Object.keys(globals.connectedUsers)) {
-        logger.info(`${i}) ${userID} - ${globals.connectedUsers[userID].username}`);
-        i += 1;
+    if (Object.keys(globals.connectedUsers).length === 0) {
+        logger.info('n/a - No users connected.');
+    } else {
+        let i = 1;
+        for (const userID of Object.keys(globals.connectedUsers)) {
+            logger.info(`${i}) ${userID} - ${globals.connectedUsers[userID].username}`);
+            i += 1;
+        }
     }
 
     logger.info('globals.currentGames:');
-    i = 1;
-    for (const gameID of Object.keys(globals.currentGames)) {
-        logger.info(`${i}) ${gameID} - ${globals.currentGames[gameID].name}`);
-        i += 1;
+    if (Object.keys(globals.currentGames).length === 0) {
+        logger.info('n/a - No games created.');
+    } else {
+        let i = 1;
+        for (const gameID of Object.keys(globals.currentGames)) {
+            logger.info(`${i}) ${gameID} - ${globals.currentGames[gameID].name}`);
+            i += 1;
+        }
     }
 };
