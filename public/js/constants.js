89aa99f73e86626580cc05b482b84d07ae3e3a9d
J. Rogers <rogers-j@users.noreply.github.com>
2017-10-13 12:09:10 -0700

    Refactor diamond arc calculation to specify size
    The function starts from the desired sizes and uses them to generate
    a set of eight points. These points are used to draw the arcs that
    make the diamond.
    
    Note that the sizes cannot currently be adjusted or the diamond will
    not be centered where it appears.

--- public/js/constants.js
+++ public/js/constants.js
@@ -42,12 +42,34 @@
     exports.PATHFUNC.set(
         exports.SHAPE.DIAMOND,
         (ctx) => {
+            const w = 75;
+            const h = 100;
+            const points = [
+                [1, 0],
+                [2, 1],
+                [1, 2],
+                [0, 1],
+            ]
+            .map(point => [point[0] * w, point[1] * h]);
+            const curveX = 1.46;
+            const curveY = 0.6;
+            const interps = [
+                [0, 0],
+                [0, 1],
+                [1, 1],
+                [1, 0],
+            ]
+            .map(v => [
+                [curveX, 2 - curveX][v[0]] * w,
+                [curveY, 2 - curveY][v[1]] * h,
+            ]);
+
             ctx.beginPath();
-            ctx.moveTo(75, 0);
-            ctx.quadraticCurveTo(110, 60, 150, 100);
-            ctx.quadraticCurveTo(110, 140, 75, 200);
-            ctx.quadraticCurveTo(40, 140, 0, 100);
-            ctx.quadraticCurveTo(40, 60, 75, 0);
+            ctx.moveTo(...points[0]);
+            ctx.quadraticCurveTo(...interps[0], ...points[1]);
+            ctx.quadraticCurveTo(...interps[1], ...points[2]);
+            ctx.quadraticCurveTo(...interps[2], ...points[3]);
+            ctx.quadraticCurveTo(...interps[3], ...points[0]);
         },
     );
     exports.PATHFUNC.set(
