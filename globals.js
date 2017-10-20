ccae41db6f60e0193499a464efba4f2017ad9af8
James Nesta <j.nesta@gmail.com>
2017-10-20 21:01:28 +0000

    adding port based on env

--- globals.js
+++ globals.js
@@ -1,5 +1,5 @@
 // Server configuration
-const port = 3000;
+const port = process.env.PORT || 3000;
 const startingTime = 5 * 60 * 1000; // In milliseconds
 const extraTurnTime = 10 * 1000; // In milliseconds
 
