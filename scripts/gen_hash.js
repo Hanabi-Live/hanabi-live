45ecc92b4f4fb3e323bf6ec4cc04bebe019f7361
Zamiell <j.nesta@gmail.com>
2018-06-30 18:00:17 +0000

    updating password script

--- scripts/gen_hash.js
+++ scripts/gen_hash.js
@@ -2,12 +2,15 @@
 const prompt = require('prompt');
 const sha256 = require('./sha256');
 
+// Constants
+const prefix = 'Hanabi password ';
+
 // Prompt for input
 prompt.get([{
     name: 'password',
     required: true,
-    hidden: true,
+    //hidden: true,
 }], (err, result) => {
-    const hash = sha256(result.password);
+    const hash = sha256(prefix + result.password);
     console.log(hash);
 });
