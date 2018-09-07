63a40c1eece4b3d1cceb11e95c7a635b9e88a538
J. Rogers <rogers-j@users.noreply.github.com>
2018-09-06 21:54:03 -0700

    Update pull script to reset head to master for same file contents
    Prevent changes to server files when on a branch other than master.
    This is considered a failure of the script.
    
    If the current commit hash is identical to the origin's, return.
    This is considered a success since no action is needed.
    
    If the hashes are not identical but the file contents are, reset
    to match origin.
    
    Only pull if the HEAD commit is an ancestor to the tip of origin.
    This ensures that an automated merge or rebase does not leave the
    server in a broken state.

--- pull.sh
+++ pull.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
 cd "/root/go/src/github.com/Zamiell/hanabi-live/"
-git pull --rebase
+git fetch
+
+if [ $(git rev-parse HEAD) = $(git rev-parse origin/master) ]; then
+        exit 0
+fi
+
+if [ $(git symbolic-ref --short HEAD) != "master" ]; then
+        exit 1
+fi
+
+if git diff origin/master --quiet --exit-code; then
+        git reset --hard origin/master
+else
+        git pull --ff-only
+fi
