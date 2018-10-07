04234635ec56d47f1c48b5a291f26efef8fcdcc2
Zamiell <j.nesta@gmail.com>
2018-10-07 16:58:09 -0700

    Add detail to debug messages to show game member state
    For #146

--- src/debug.go
+++ src/debug.go
@@ -31,32 +31,32 @@ func debug(s *Session, d *CommandData) {
 		typeOfT := s.Type()
 		for i := 0; i < s.NumField(); i++ {
 			f := s.Field(i)
-			line := fmt.Sprintf("    %s\t= %v\n", typeOfT.Field(i).Name, f.Interface())
+			line := fmt.Sprintf("  %s\t= %v\n", typeOfT.Field(i).Name, f.Interface())
 			log.Debug(line)
 		}
 		log.Debug("\n")
 
 		// Manually enumerate the slices and maps
-		log.Debug("    Players:")
+		log.Debug("  Players:")
 		for j, p := range g.Players { // This is a []*Player
-			log.Debug("        " + strconv.Itoa(j) + " - " + p.Name)
+			log.Debug("    " + strconv.Itoa(j) + " - User ID: " + strconv.Itoa(p.ID) + ", Username: " + p.Name)
 		}
 		if len(g.Players) == 0 {
-			log.Debug("        [no players]")
+			log.Debug("    [no players]")
 		}
-		log.Debug("    Spectators:")
+		log.Debug("  Spectators:")
 		for j, s := range g.Spectators { // This is a []*Session
-			log.Debug("        " + strconv.Itoa(j) + " - " + s.Username())
+			log.Debug("    " + strconv.Itoa(j) + " - User ID: " + strconv.Itoa(s.UserID()) + ", Username: " + s.Username())
 		}
 		if len(g.Spectators) == 0 {
-			log.Debug("        [no spectators]")
+			log.Debug("    [no spectators]")
 		}
-		log.Debug("    DisconSpectators:")
-		for j := range g.DisconSpectators { // This is a map[int]*bool
-			log.Debug("        " + strconv.Itoa(j))
+		log.Debug("  DisconSpectators:")
+		for k, v := range g.DisconSpectators { // This is a map[int]*bool
+			log.Debug("        User ID: " + strconv.Itoa(k) + " - " + strconv.FormatBool(v))
 		}
 		if len(g.DisconSpectators) == 0 {
-			log.Debug("        [no disconnected spectators]")
+			log.Debug("    [no disconnected spectators]")
 		}
 	}
 
@@ -64,16 +64,16 @@ func debug(s *Session, d *CommandData) {
 	log.Debug("---------------------------------------------------------------")
 	log.Debug("Current users:")
 	for i, s2 := range sessions { // This is a map[int]*Session
-		log.Debug(strconv.Itoa(i) + " - " + s2.Username() + " - " + s2.Status() + " - " + strconv.Itoa(s2.CurrentGame()))
+		log.Debug("  User ID: " + strconv.Itoa(i) + ", Username: " + s2.Username() + ", Status: " + s2.Status() + ", Current game: " + strconv.Itoa(s2.CurrentGame()))
 	}
 
 	// Print out the waiting list
 	log.Debug("---------------------------------------------------------------")
 	log.Debug("Waiting list:")
 	for i, p := range waitingList { // This is a []*models.Waiter
-		log.Debug(strconv.Itoa(i) + " - " + p.Username + " - " + p.DiscordMention + " - " + p.DatetimeExpired.String())
+		log.Debug("  " + strconv.Itoa(i) + " - " + p.Username + " - " + p.DiscordMention + " - " + p.DatetimeExpired.String())
 	}
-	log.Debug("discordLastAtHere:", discordLastAtHere)
+	log.Debug("  discordLastAtHere:", discordLastAtHere)
 
 	log.Debug("---------------------------------------------------------------")
 }
