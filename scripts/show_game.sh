91493ece4def31485efa2da1ab87b77694718c4c
Zamiell <j.nesta@gmail.com>
2019-01-11 03:13:35 +0000

    fixing link

--- scripts/show_game.sh
+++ scripts/show_game.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 # Configuration
-USER="Zamiel"
-SCORE="30"
-VARIANT="Black & Dark Rainbow (6 Suits)"
+GAME_ID="17232"
 
 # Import the database username and password
 source ../.env
 
 mysql -u$DB_USER -p$DB_PASS $DB_NAME <<EOF
-SELECT games.id AS id, games.seed AS seed
+SELECT games.id, games.name
 FROM games
 JOIN game_participants ON games.id = game_participants.game_id
 JOIN users ON users.id = game_participants.user_id
-WHERE games.score = $SCORE
-AND games.variant = "$VARIANT"
-AND users.username = "$USER"
+WHERE games.id = $GAME_ID
 ;
 EOF
