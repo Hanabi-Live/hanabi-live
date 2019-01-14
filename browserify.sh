f787d401adb65f64b25d496b7851a22f9487c20d
Zamiell <j.nesta@gmail.com>
2019-01-13 21:38:09 -0500

    new script

---
+++ browserify.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Get the directory of this script
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+browserify "$DIR/public/js/src/main.js" --outfile "$DIR/public/js/main.bundled.js" --verbose --debug
