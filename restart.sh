90545ca60d5f1b7be6f63ab125d96be1de4db682
Zamiell <j.nesta@gmail.com>
2019-01-14 01:24:08 +0000

    pushing dev code to live

--- restart.sh
+++ restart.sh
@@ -15,4 +15,4 @@ else
 fi
 
 # Browserify the client code
-browserify "$DIR/public/js/dev2/main.js" --outfile "$DIR/public/js/main.bundled.js" --verbose --debug
+browserify "$DIR/public/js/src/main.js" --outfile "$DIR/public/js/main.bundled.js" --verbose --debug
--- watchify.sh
+++ watchify.sh
@@ -4,4 +4,4 @@
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-watchify "$DIR/public/js/dev2/main.js" --outfile "$DIR/public/js/main.bundled.js" --verbose --debug
+watchify "$DIR/public/js/src/main.js" --outfile "$DIR/public/js/main.bundled.js" --verbose --debug
