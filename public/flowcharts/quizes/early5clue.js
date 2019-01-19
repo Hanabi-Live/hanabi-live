b181f32196c1dd3b78892cbe2fb3030d5acdab07
Zamiell <j.nesta@gmail.com>
2019-01-19 01:00:52 -0500

    cleaning up the flowchart code

--- public/flowcharts/quizes/early5clue.js
+++ public/flowcharts/quizes/early5clue.js
@@ -1,12 +1,12 @@
 var input = [{
     "slug": "Start",
-    "text": "Was the right-most 5 on chop?",
+    "text": "It is the <i>Early Game</i> and one or more 5's are clued with number 5.<br /><br />Was the right-most 5 on chop?",
     "connectsto": "save, otherclues",
     "connectstext": "Yes, No",
     "rowNumber": 1
 }, {
     "slug": "save",
-    "text": "It is a <i>Save Clue</i> on the 5 (This does not imply anything special)",
+    "text": "It is a <i>Save Clue</i> on the 5. (This does not imply anything special.)",
     "connectsto": "End",
     "connectstext": "The End",
     "rowNumber": 2
@@ -18,7 +18,7 @@ var input = [{
     "rowNumber": 3
 }, {
     "slug": "onlyfiness",
-    "text": "Was the only thing left to do giving a <i>Play Clue</i> to a card on <i>Finesse Position</i>?",
+    "text": "Was the only thing left to do to give a <i>Play Clue</i> to a card on <i>Finesse Position</i>?",
     "connectsto": "other5scloser, oneaway",
     "connectstext": "Yes, No",
     "rowNumber": 4
@@ -30,13 +30,13 @@ var input = [{
     "rowNumber": 5
 }, {
     "slug": "early5cm",
-    "text": "It is an <i>Early 5's Chop Move</i> (They should Chop Move the card to the right of the 5)",
+    "text": "It is an <i>Early 5's Chop Move</i>. (They should <i>Chop Move</i> the card to the right of the 5.)",
     "connectsto": "End",
     "connectstext": "The End",
     "rowNumber": 6
 }, {
     "slug": "5pull",
-    "text": "It is a <i>5 Pull</i> (They should blind play the card to the right of the 5)",
+    "text": "It is a <i>5 Pull</i>. (They should blind play the card to the right of the 5.)",
     "connectsto": "End",
     "connectstext": "The End",
     "rowNumber": 7
@@ -48,13 +48,13 @@ var input = [{
     "rowNumber": 8
 }, {
     "slug": "5stall",
-    "text": "It is a <i>5 Stall</i> (This is an early save on the 5 and does not imply anything special)",
+    "text": "It is a <i>5 Stall</i>. (This is an early save on the 5 and does not imply anything special.)",
     "connectsto": "End",
     "connectstext": "The End",
     "rowNumber": 9
 }, {
     "slug": "End",
-    "text": "Back to the hanabi dungeons with you",
+    "text": "Back to the Hanabi dungeons with you.",
     "connectsto": "",
     "connectstext": "",
     "rowNumber": 10
