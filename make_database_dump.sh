510a817813385455ad0cad86482d4c98409c03e4
James <j.nesta@gmail.com>
2019-01-29 20:37:42 -0500

    Update make_database_dump.sh

--- make_database_dump.sh
+++ make_database_dump.sh
@@ -5,12 +5,13 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Configuration
+BACKUPS_DIR="$DIR/backups"
 FILENAME=hanabi-live-`date +%s`.sql # "date +%s" returns the epoch timestamp
 
 # Import the database username and password
 source "$DIR/.env"
 
 # Back up the database and gzip it
-mkdir -p "$DIR/backups"
-mysqldump -u"$DB_USER" -p"$DB_PASS" hanabi > "$DIR/backups/$FILENAME"
-gzip "$DIR/backups/$FILENAME"
+mkdir -p "$BACKUPS_DIR"
+mysqldump -u"$DB_USER" -p"$DB_PASS" "$DB_NAME" > "$BACKUPS_DIR/$FILENAME"
+gzip "$BACKUPS_DIR/$FILENAME"
