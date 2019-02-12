499173b54c0ec47a225a32150c672fa5509eedde
Zamiell <j.nesta@gmail.com>
2019-02-12 01:32:14 -0500

    fixes for windows

--- public/js/Gruntfile.js
+++ public/js/Gruntfile.js
@@ -1,10 +1,11 @@
 module.exports = (grunt) => {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+        'pkg': grunt.file.readJSON('package.json'),
 
-        // "Browserify" the Hanabi JavaScript (to convert Node-style imports to compatible browser code)
+        // "Browserify" the Hanabi JavaScript
+        // (to convert Node-style imports to compatible browser code)
         // https://github.com/browserify/browserify
-        browserify: {
+        'browserify': {
             dist: {
                 src: 'src/main.js',
                 dest: 'main.browserified.js',
@@ -13,7 +14,7 @@ module.exports = (grunt) => {
 
         // Concatenate all of the JavaScript files together into the "main.js" file
         // Concatenate all of the CSS files together into the "main.css" file
-        concat: {
+        'concat': {
             js: {
                 src: [
                     'lib/jquery-3.3.1.min.js',
@@ -42,14 +43,19 @@ module.exports = (grunt) => {
 
         // Transpile the JavaScript using Babel so that it will work in older browsers
         // (Babel options are configured in the ".babelrc" file)
-        babel: {
+        'babel': {
             options: {
+                // Since we are transpiling a file greater than 500 thousand lines,
+                // Babel will throw a warning unless we explicitly define this
+                compact: true,
+
                 // This is needed to tell Babel that we want maximum transpilation
                 // (so that the resulting JavaScript will work on every single browser)
                 // https://babeljs.io/docs/en/babel-preset-env
                 presets: ['@babel/preset-env'],
 
-                // Babel assumes that JavaScript is ES6 modules by default, which is not the case here
+                // Babel assumes that JavaScript is ES6 modules by default,
+                // which is not the case here
                 // https://stackoverflow.com/questions/34973442/how-to-stop-babel-from-transpiling-this-to-undefined-and-inserting-use-str
                 sourceType: 'unambiguous',
             },
@@ -64,11 +70,16 @@ module.exports = (grunt) => {
             dist: {
                 src: 'main.transpiled.js',
                 dest: 'main.min.js',
+                options: {
+                    // The compiler echos the JavaScript output unless
+                    // we explicitly tell it to be quiet
+                    warning_level: 'QUIET',
+                },
             },
         },
 
         // Minify the CSS
-        cssmin: {
+        'cssmin': {
             options: {
                 // clean-css only does level 1 optimizations by default
                 // https://github.com/jakubpawlowicz/clean-css#optimization-levels
@@ -85,7 +96,10 @@ module.exports = (grunt) => {
     grunt.loadNpmTasks('grunt-babel');
     // The Grunt plugin for the Google Closure Compiler is documented here:
     // https://github.com/google/closure-compiler-npm/blob/master/packages/google-closure-compiler/docs/grunt.md
-    require('google-closure-compiler').grunt(grunt);
+    require('google-closure-compiler').grunt(grunt, { // eslint-disable-line global-require
+        // We must use the Java version or else we get "JavaScript heap out of memory" errors
+        platform: ['java'],
+    });
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.registerTask('default', [
         'browserify',
