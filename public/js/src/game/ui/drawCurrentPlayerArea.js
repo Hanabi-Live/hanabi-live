c7de44b60c18f326167f7a285174bba9f1fcef42
Zamiell <j.nesta@gmail.com>
2019-03-19 11:03:39 -0400

    splitting up drawUI into separate files

---
+++ public/js/src/game/ui/drawCurrentPlayerArea.js
@@ -0,0 +1,280 @@
+// Imports
+const FitText = require('./FitText');
+const globals = require('./globals');
+const graphics = require('./graphics');
+
+module.exports = (clueAreaValues) => {
+    // Constants
+    const winW = globals.stage.getWidth();
+    const winH = globals.stage.getHeight();
+
+    // The "Current player: [player name]" box
+    const currentPlayerAreaWidth = 0.3; // This is big enough to fit in between the two timers
+    const currentPlayerAreaValues = {
+        x: clueAreaValues.x + (clueAreaValues.w / 2) - (currentPlayerAreaWidth / 2),
+        y: clueAreaValues.y + 0.015,
+        w: currentPlayerAreaWidth,
+        h: 0.15,
+        spacing: 0.006,
+    };
+    globals.elements.currentPlayerArea = new graphics.Group({
+        x: currentPlayerAreaValues.x * winW,
+        y: currentPlayerAreaValues.y * winH,
+        height: currentPlayerAreaValues.h * winH,
+        visible: !globals.replay,
+    });
+    globals.layers.UI.add(globals.elements.currentPlayerArea);
+
+    let currentPlayerBox1Width = (currentPlayerAreaValues.w * 0.75);
+    currentPlayerBox1Width -= currentPlayerAreaValues.spacing;
+    globals.elements.currentPlayerRect1 = new graphics.Rect({
+        width: currentPlayerBox1Width * winW,
+        height: currentPlayerAreaValues.h * winH,
+        cornerRadius: 0.01 * winW,
+        fill: 'black',
+        opacity: 0.2,
+    });
+    globals.elements.currentPlayerArea.add(globals.elements.currentPlayerRect1);
+
+    const textValues = {
+        w: currentPlayerBox1Width - (currentPlayerAreaValues.spacing * 4),
+        w2: currentPlayerBox1Width - (currentPlayerAreaValues.spacing * 2),
+    };
+    textValues.x = (currentPlayerBox1Width / 2) - (textValues.w / 2);
+    textValues.x2 = (currentPlayerBox1Width / 2) - (textValues.w2 / 2);
+
+    globals.elements.currentPlayerText1 = new FitText({
+        x: textValues.x * winW,
+        width: textValues.w * winW,
+        fontFamily: 'Verdana',
+        fontSize: 0.08 * winH,
+        text: 'Current player:',
+        align: 'center',
+        fill: globals.labelColor,
+        shadowColor: 'black',
+        shadowBlur: 10,
+        shadowOffset: {
+            x: 0,
+            y: 0,
+        },
+        shadowOpacity: 0.9,
+        listening: false,
+    });
+    globals.elements.currentPlayerArea.add(globals.elements.currentPlayerText1);
+
+    globals.elements.currentPlayerText2 = new FitText({
+        x: textValues.x * winW,
+        width: textValues.w * winW,
+        fontFamily: 'Verdana',
+        fontSize: 0.08 * winH,
+        text: '',
+        align: 'center',
+        shadowColor: 'black',
+        shadowBlur: 10,
+        shadowOffset: {
+            x: 0,
+            y: 0,
+        },
+        shadowOpacity: 0.9,
+        listening: false,
+    });
+    globals.elements.currentPlayerArea.add(globals.elements.currentPlayerText2);
+    globals.elements.currentPlayerText2.setPlayer = function set(currentPlayerIndex, threeLines) {
+        if (globals.ourTurn && !globals.spectating) {
+            this.setText('You');
+            this.setFill('yellow');
+        } else {
+            const text = globals.playerNames[currentPlayerIndex] || 'Undefined';
+            this.setText(text);
+            this.setFill('#ffffcc');
+        }
+        let maxSize = (currentPlayerAreaValues.h / 3) * winH;
+        if (threeLines) {
+            maxSize = (currentPlayerAreaValues.h / 4) * winH;
+        }
+        this.setWidth(textValues.w * winW);
+        this.resize();
+        while (this._getTextSize(this.getText()).height > maxSize) {
+            this.setWidth(this.getWidth() * 0.9);
+            this.resize();
+        }
+        this.setX((globals.elements.currentPlayerRect1.getWidth() / 2) - (this.getWidth() / 2));
+    };
+
+    globals.elements.currentPlayerText3 = new FitText({
+        x: textValues.x2 * winW,
+        width: textValues.w2 * winW,
+        fontFamily: 'Verdana',
+        fontSize: 0.08 * winH,
+        text: '',
+        align: 'center',
+        fill: 'red',
+        shadowColor: 'black',
+        shadowBlur: 10,
+        shadowOffset: {
+            x: 0,
+            y: 0,
+        },
+        shadowOpacity: 0.9,
+        listening: false,
+        visible: false,
+    });
+    globals.elements.currentPlayerArea.add(globals.elements.currentPlayerText3);
+
+    const arrowValues = {
+        x: (currentPlayerAreaValues.w * 0.75) + currentPlayerAreaValues.spacing,
+        w: (currentPlayerAreaValues.w * 0.25) - currentPlayerAreaValues.spacing,
+        h: currentPlayerAreaValues.h,
+        spacing: 0.01,
+    };
+    const rect2 = new graphics.Rect({
+        x: arrowValues.x * winW,
+        width: arrowValues.w * winW,
+        height: currentPlayerAreaValues.h * winH,
+        cornerRadius: 0.005 * winW,
+        fill: 'black',
+        opacity: 0.2,
+    });
+    globals.elements.currentPlayerArea.add(rect2);
+
+    globals.elements.currentPlayerArrow = new graphics.Group({
+        x: (arrowValues.x + (arrowValues.w / 2)) * winW,
+        y: (currentPlayerAreaValues.h / 2) * winH,
+        offset: {
+            x: (arrowValues.x + (arrowValues.w / 2) * winW),
+            y: (currentPlayerAreaValues.h / 2) * winH,
+        },
+        listening: false,
+    });
+    globals.elements.currentPlayerArea.add(globals.elements.currentPlayerArrow);
+
+    const arrowBorder = new graphics.Arrow({
+        points: [
+            arrowValues.spacing * winW,
+            (arrowValues.h / 2) * winH,
+            (arrowValues.w - arrowValues.spacing) * winW,
+            (arrowValues.h / 2) * winH,
+        ],
+        pointerLength: 10,
+        pointerWidth: 10,
+        fill: 'black',
+        stroke: 'black',
+        strokeWidth: 10,
+        shadowBlur: 75,
+        shadowOpacity: 1,
+        listening: false,
+    });
+    globals.elements.currentPlayerArrow.add(arrowBorder);
+
+    const arrowBorderEdge = new graphics.Line({
+        points: [
+            (arrowValues.spacing) * winW,
+            ((arrowValues.h / 2) - 0.005) * winH,
+            (arrowValues.spacing) * winW,
+            ((arrowValues.h / 2) + 0.005) * winH,
+        ],
+        fill: 'black',
+        stroke: 'black',
+        strokeWidth: 5,
+    });
+    globals.elements.currentPlayerArrow.add(arrowBorderEdge);
+
+    const arrowMain = new graphics.Arrow({
+        points: [
+            arrowValues.spacing * winW,
+            (arrowValues.h / 2) * winH,
+            (arrowValues.w - arrowValues.spacing) * winW,
+            (arrowValues.h / 2) * winH,
+        ],
+        pointerLength: 10,
+        pointerWidth: 10,
+        fill: globals.labelColor,
+        stroke: globals.labelColor,
+        strokeWidth: 5,
+        listening: false,
+    });
+    globals.elements.currentPlayerArrow.add(arrowMain);
+
+    // Set the "Current Player" area up for this specific turn,
+    // which will always be either 2 or 3 lines long
+    globals.elements.currentPlayerArea.update = function update(currentPlayerIndex) {
+        this.setVisible(
+            // Don't show it if we are in a solo/shared replay
+            // or if we happen to have the in-game replay open
+            !globals.inReplay
+            // Don't show it if the clue UI is supposed to be there
+            && (!globals.ourTurn || globals.clues === 0)
+            && currentPlayerIndex !== -1, // Don't show it if this is the end of the game
+        );
+
+        if (currentPlayerIndex === -1) {
+            // The game has ended
+            return;
+        }
+
+        // Update the text
+        const text1 = globals.elements.currentPlayerText1;
+        const text2 = globals.elements.currentPlayerText2;
+        const text3 = globals.elements.currentPlayerText3;
+        let specialText = '';
+        if (globals.clues === 0) {
+            specialText = '(cannot clue; 0 clues left)';
+            text3.setFill('red');
+        } else if (globals.clues === 8) {
+            specialText = '(cannot discard; at 8 clues)';
+            text3.setFill('red');
+        } else if (globals.elements.playerHands[currentPlayerIndex].isLocked()) {
+            specialText = '(locked; may not be able to discard)';
+            text3.setFill('yellow');
+        } else if (globals.elements.noDoubleDiscardLabel.getVisible()) {
+            specialText = '(potentially in a "Double Discard" situation)';
+            text3.setFill('yellow');
+        }
+        const totalH = this.getHeight();
+        const text1H = text1._getTextSize(text1.getText()).height;
+        if (specialText === '') {
+            // 2 lines
+            text2.setPlayer(currentPlayerIndex, false);
+            const text2H = text2._getTextSize(text2.getText()).height;
+            const spacing = 0.03 * globals.stage.getHeight();
+            text1.setY((totalH / 2) - (text1H / 2) - spacing);
+            text2.setY((totalH / 2) - (text2H / 2) + spacing);
+            text3.hide();
+        } else {
+            // 3 lines
+            text2.setPlayer(currentPlayerIndex, true);
+            const text2H = text2._getTextSize(text2.getText()).height;
+            const spacing = 0.04 * globals.stage.getHeight();
+            text1.setY((totalH / 2) - (text1H / 2) - spacing);
+            text2.setY((totalH / 2) - (text2H / 2) + (spacing * 0.25));
+            text3.setY((totalH / 2) - (text1H / 2) + (spacing * 1.5));
+            text3.setText(specialText);
+            text3.show();
+        }
+
+        // Make the arrow point to the current player
+        const centerPos = globals.elements.playerHands[currentPlayerIndex].getAbsoluteCenterPos();
+        const thisPos = globals.elements.currentPlayerArrow.getAbsolutePosition();
+        const x = centerPos.x - thisPos.x;
+        const y = centerPos.y - thisPos.y;
+        const radians = Math.atan(y / x);
+        let rotation = radians * (180 / Math.PI);
+        if (x < 0) {
+            rotation += 180;
+        }
+
+        if (globals.animateFast) {
+            globals.elements.currentPlayerArrow.setRotation(rotation);
+        } else {
+            if (globals.elements.currentPlayerArrowTween) {
+                globals.elements.currentPlayerArrowTween.destroy();
+            }
+            globals.elements.currentPlayerArrowTween = new graphics.Tween({
+                node: globals.elements.currentPlayerArrow,
+                duration: 0.75,
+                rotation,
+                runonce: true,
+            }).play();
+        }
+    };
+};
---
+++ public/js/src/game/ui/drawReplayArea.js
@@ -0,0 +1,266 @@
+// Imports
+const Button = require('./Button');
+const FitText = require('./FitText');
+const globals = require('./globals');
+const graphics = require('./graphics');
+const hypothetical = require('./hypothetical');
+const replay = require('./replay');
+const ToggleButton = require('./ToggleButton');
+
+module.exports = () => {
+    // Constants
+    const winW = globals.stage.getWidth();
+    const winH = globals.stage.getHeight();
+
+    const replayAreaValues = {
+        x: 0.15,
+        y: 0.51,
+        w: 0.5,
+    };
+    if (globals.lobby.settings.showBGAUI) {
+        replayAreaValues.x = 0.01;
+        replayAreaValues.y = 0.49;
+        replayAreaValues.w = 0.4;
+    }
+    globals.elements.replayArea = new graphics.Group({
+        x: replayAreaValues.x * winW,
+        y: replayAreaValues.y * winH,
+        width: replayAreaValues.w * winW,
+        height: 0.27 * winH,
+    });
+
+    // The thin black rectangle that the replay slider slides on
+    const replayBar = new graphics.Rect({
+        x: 0,
+        y: 0.0425 * winH,
+        width: replayAreaValues.w * winW,
+        height: 0.01 * winH,
+        fill: 'black',
+        cornerRadius: 0.005 * winH,
+        listening: false,
+    });
+    globals.elements.replayArea.add(replayBar);
+
+    // An invisible rectangle over the visible black rectangle above
+    // (which is slightly bigger so that it is easier to click on)
+    const replayBarClickRect = new graphics.Rect({
+        x: 0,
+        y: 0,
+        width: replayAreaValues.w * winW,
+        height: 0.05 * winH,
+        opacity: 0,
+    });
+    replayBarClickRect.on('click', replay.barClick);
+    globals.elements.replayArea.add(replayBarClickRect);
+
+    const shuttleValues = {
+        x: 0,
+        y: 0.0325,
+        w: 0.03,
+        h: 0.03,
+        cornerRadius: 0.01,
+        stroke: 3,
+    };
+
+    // The shared (white) replay shuttle
+    // (we want it to be below the normal replay shuttle, so we define it first)
+    globals.elements.replayShuttleShared = new graphics.Rect({
+        x: shuttleValues.x,
+        y: shuttleValues.y * winH,
+        width: shuttleValues.w * winW,
+        height: shuttleValues.h * winH,
+        cornerRadius: shuttleValues.cornerRadius * winW,
+        fill: '#d1d1d1', // Gray
+        stroke: shuttleValues.stroke,
+        visible: !globals.useSharedTurns,
+    });
+    globals.elements.replayShuttleShared.on('click tap', () => {
+        // This is needed because the shared replay shuttle will block the replay bar
+        replay.goto(globals.sharedReplayTurn, true);
+    });
+    globals.elements.replayArea.add(globals.elements.replayShuttleShared);
+
+    // This is the normal (blue) replay shuttle
+    globals.elements.replayShuttle = new graphics.Rect({
+        x: shuttleValues.x,
+        y: shuttleValues.y * winH,
+        width: shuttleValues.w * winW,
+        height: shuttleValues.h * winH,
+        fill: '#0000cc', // Blue
+        cornerRadius: shuttleValues.cornerRadius * winW,
+        draggable: true,
+        dragBoundFunc: replay.barDrag,
+        stroke: shuttleValues.stroke,
+    });
+    globals.elements.replayArea.add(globals.elements.replayShuttle);
+
+    replay.adjustShuttles();
+
+    const replayButtonValues = {
+        x: 0.1,
+        y: 0.07,
+        w: 0.06,
+        h: 0.08,
+        spacing: 0.02,
+    };
+    if (globals.lobby.settings.showBGAUI) {
+        replayButtonValues.x = 0.05;
+    }
+
+    {
+        let { x } = replayButtonValues;
+
+        // Go back to the beginning (the left-most button)
+        globals.elements.replayBackFullButton = new Button({
+            x: x * winW,
+            y: 0.07 * winH,
+            width: replayButtonValues.w * winW,
+            height: replayButtonValues.h * winH,
+            image: 'replay-back-full',
+        });
+        globals.elements.replayBackFullButton.on('click tap', replay.backFull);
+        globals.elements.replayArea.add(globals.elements.replayBackFullButton);
+
+        // Go back one turn (the second left-most button)
+        x += replayButtonValues.w + replayButtonValues.spacing;
+        globals.elements.replayBackButton = new Button({
+            x: x * winW,
+            y: 0.07 * winH,
+            width: replayButtonValues.w * winW,
+            height: replayButtonValues.h * winH,
+            image: 'replay-back',
+        });
+        globals.elements.replayBackButton.on('click tap', replay.back);
+        globals.elements.replayArea.add(globals.elements.replayBackButton);
+
+        // Go forward one turn (the second right-most button)
+        x += replayButtonValues.w + replayButtonValues.spacing;
+        globals.elements.replayForwardButton = new Button({
+            x: x * winW,
+            y: 0.07 * winH,
+            width: replayButtonValues.w * winW,
+            height: replayButtonValues.h * winH,
+            image: 'replay-forward',
+        });
+        globals.elements.replayForwardButton.on('click tap', replay.forward);
+        globals.elements.replayArea.add(globals.elements.replayForwardButton);
+
+        // Go forward to the end (the right-most button)
+        x += replayButtonValues.w + replayButtonValues.spacing;
+        globals.elements.replayForwardFullButton = new Button({
+            x: x * winW,
+            y: 0.07 * winH,
+            width: replayButtonValues.w * winW,
+            height: replayButtonValues.h * winH,
+            image: 'replay-forward-full',
+        });
+        globals.elements.replayForwardFullButton.on('click tap', replay.forwardFull);
+        globals.elements.replayArea.add(globals.elements.replayForwardFullButton);
+    }
+
+    // The "Exit Replay" button
+    const bottomButtonValues = {
+        y: 0.17,
+    };
+    globals.elements.replayExitButton = new Button({
+        x: (replayButtonValues.x + replayButtonValues.w + (replayButtonValues.spacing / 2)) * winW,
+        y: bottomButtonValues.y * winH,
+        width: ((replayButtonValues.w * 2) + (replayButtonValues.spacing * 2)) * winW,
+        height: replayButtonValues.w * winH,
+        text: 'Exit Replay',
+        visible: !globals.replay,
+    });
+    globals.elements.replayExitButton.on('click tap', replay.exitButton);
+    globals.elements.replayArea.add(globals.elements.replayExitButton);
+
+    const extra = 0.05;
+    const bottomLeftReplayButtonValues = {
+        x: replayButtonValues.x - extra,
+        y: bottomButtonValues.y,
+        w: replayButtonValues.w * 2 + replayButtonValues.spacing + extra,
+        h: 0.06,
+    };
+
+    // The "Pause Shared Turns"  / "Use Shared Turns" button
+    // (this will be shown when the client receives the "replayLeader" command)
+    globals.elements.toggleSharedTurnButton = new ToggleButton({
+        width: bottomLeftReplayButtonValues.w * winW,
+        height: bottomLeftReplayButtonValues.h * winH,
+        text: 'Pause Shared Turns',
+        alternateText: 'Use Shared Turns',
+        initialState: !globals.useSharedTurns,
+        visible: false,
+    });
+    // It will be centered if there is only 1 button (and moved left otherwise)
+    const totalWidth = (replayButtonValues.w * 4) + (replayButtonValues.spacing * 3);
+    globals.elements.toggleSharedTurnButton.setCenter = function setCenter() {
+        const x = replayButtonValues.x + ((totalWidth - bottomLeftReplayButtonValues.w) / 2);
+        this.setX(x * winW);
+        this.setY(bottomLeftReplayButtonValues.y * winH);
+    };
+    globals.elements.toggleSharedTurnButton.setLeft = function setLeft() {
+        this.setX(bottomLeftReplayButtonValues.x * winW);
+        this.setY(bottomLeftReplayButtonValues.y * winH);
+    };
+    globals.elements.toggleSharedTurnButton.on('click tap', replay.toggleSharedTurns);
+    globals.elements.replayArea.add(globals.elements.toggleSharedTurnButton);
+
+    const bottomRightReplayButtonValues = {
+        x: replayButtonValues.x + (replayButtonValues.w * 2) + (replayButtonValues.spacing * 2),
+        y: bottomLeftReplayButtonValues.y,
+        w: bottomLeftReplayButtonValues.w,
+        h: bottomLeftReplayButtonValues.h,
+    };
+
+    // The "Enter Hypothetical" / "Exit Hypothetical" button
+    globals.elements.toggleHypoButton = new ToggleButton({
+        x: bottomRightReplayButtonValues.x * winW,
+        y: bottomRightReplayButtonValues.y * winH,
+        width: bottomRightReplayButtonValues.w * winW,
+        height: bottomRightReplayButtonValues.h * winH,
+        text: 'Enter Hypothetical',
+        alternateText: 'Exit Hypothetical',
+        initialState: globals.hypothetical,
+        visible: globals.replay && globals.amSharedReplayLeader,
+    });
+    globals.elements.toggleHypoButton.on('click tap', hypothetical.toggle);
+    globals.elements.replayArea.add(globals.elements.toggleHypoButton);
+
+    // The "Hypothetical" circle that shows whether or not we are currently in a hypothetical
+    globals.elements.hypoCircle = new graphics.Group({
+        x: bottomRightReplayButtonValues.x * winW,
+        y: bottomRightReplayButtonValues.y * winH,
+        visible: globals.hypothetical && !globals.amSharedReplayLeader,
+    });
+    globals.elements.replayArea.add(globals.elements.hypoCircle);
+
+    const circle = new graphics.Ellipse({
+        x: 0.085 * winW,
+        y: 0.03 * winH,
+        radiusX: 0.08 * winW,
+        radiusY: 0.03 * winH,
+        fill: 'black',
+        opacity: 0.5,
+        stroke: 'black',
+        strokeWidth: 4,
+    });
+    globals.elements.hypoCircle.add(circle);
+
+    const text = new FitText({
+        name: 'text',
+        x: 0.027 * winW,
+        y: 0.016 * winH,
+        width: bottomRightReplayButtonValues.w * 0.65 * winW,
+        listening: false,
+        fontSize: 0.5 * winH,
+        fontFamily: 'Verdana',
+        fill: 'yellow',
+        align: 'center',
+        text: 'Hypothetical',
+    });
+    globals.elements.hypoCircle.add(text);
+
+    // Add the replay area to the UI
+    globals.elements.replayArea.hide();
+    globals.layers.UI.add(globals.elements.replayArea);
+};
