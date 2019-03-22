1668e94b746f842de65e80040ca717dc6fa1b78e
Zamiell <j.nesta@gmail.com>
2019-03-22 17:03:10 +0000

    bugfix for go 1.12 + not building client on graceful

--- build_client.sh
+++ build_client.sh
@@ -6,6 +6,11 @@ set -e # Exit on any errors
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# Add the NPM directory to the path
+VERSION_DIRS=(/root/.nvm/versions/node/*)
+NPM_BIN_DIR=${VERSION_DIRS[-1]}/bin
+export PATH=$NPM_BIN_DIR:$PATH
+
 # Run the Grunt runner to prepare the JavaScript and the CSS
 cd "$DIR/public/js"
 npx grunt
--- restart.sh
+++ restart.sh
@@ -4,8 +4,11 @@
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# For non-interactive shells, $HOME must be specified or it will result in a cache error when compiling the Go code
+export HOME=/root
+
 # Rebuild the client code
-bash "$DIR/build_client.sh"
+"$DIR/build_client.sh"
 
 # Recompile the Golang code and restart the service
 cd "$DIR/src"
