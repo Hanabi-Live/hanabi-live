cc0d941d2feb802c872381403dac4597be4de4c4
Zamiell <j.nesta@gmail.com>
2019-03-26 11:46:31 -0400

    part 3 of previous commit

--- src/speedrun.go
+++ src/speedrun.go
@@ -1,6 +1,9 @@
 package main
 
-import "strconv"
+import (
+	"database/sql"
+	"strconv"
+)
 
 var (
 	fastestTimes             = make(map[string][]int)
@@ -16,13 +19,15 @@ var (
 func speedrunInit() {
 	for _, variant := range officialSpeedrunVariants {
 		fastestTimes[variant] = make([]int, 6)
-		perfectScore := 5 * len(variants[variant].Suits)
+		perfectScore := 5 * len(variants[variant].Suits) // Assuming 5 points per stack
 		for numPlayers := 2; numPlayers <= 5; numPlayers++ {
 			if v, err := db.Games.GetFastestTime(
 				variants[variant].ID,
 				numPlayers,
 				perfectScore,
-			); err != nil {
+			); err == sql.ErrNoRows {
+				fastestTimes[variant][numPlayers] = 60 * 60 // Default to 1 hour
+			} else if err != nil {
 				log.Fatal("Failed to get the fastest time for variant \""+variant+"\" "+
 					"with "+strconv.Itoa(numPlayers)+":", err)
 				return
