bba1c1fe2419b3dad6a7f6518f3b89cbcf8d270f
Zamiell <j.nesta@gmail.com>
2019-04-13 16:44:17 -0400

    small comment update

--- src/websocketConnect.go
+++ src/websocketConnect.go
@@ -187,8 +187,7 @@ func websocketConnect(ms *melody.Session) {
 			})
 			// (this function does not care what their current game and/or status is)
 
-			// If the user happens to be in both a game and a replay,
-			// then ignore the replay
+			// If the user happens to be in both a game and a replay, then ignore the replay
 			return
 		}
 	}
