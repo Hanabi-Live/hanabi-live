db98d01f2d1aa9ec6315ebaaa0e76872b6891f69
Zamiell <j.nesta@gmail.com>
2019-09-18 21:28:27 -0400

    bugfix for throw it in a hole

--- public/js/src/game/ui/drawUI.js
+++ public/js/src/game/ui/drawUI.js
@@ -1088,6 +1088,7 @@ const drawStatistics = () => {
         x: 0.825 * winW,
         y: 0.54 * winH,
         fontSize: 0.02 * winH,
+        visible: !globals.variant.name.startsWith('Throw It in a Hole') || globals.replay,
         listening: true,
     });
     globals.layers.UI.add(paceTextLabel);
@@ -1105,6 +1106,7 @@ const drawStatistics = () => {
         x: 0.9 * winW,
         y: 0.54 * winH,
         fontSize: 0.02 * winH,
+        visible: !globals.variant.name.startsWith('Throw It in a Hole') || globals.replay,
         listening: true,
     });
     globals.layers.UI.add(paceNumberLabel);
@@ -1122,6 +1124,7 @@ const drawStatistics = () => {
         x: 0.825 * winW,
         y: 0.56 * winH,
         fontSize: 0.02 * winH,
+        visible: !globals.variant.name.startsWith('Throw It in a Hole') || globals.replay,
         listening: true,
     });
     globals.layers.UI.add(efficiencyTextLabel);
@@ -1147,6 +1150,7 @@ const drawStatistics = () => {
         x: 0.9 * winW,
         y: 0.56 * winH,
         fontSize: 0.02 * winH,
+        visible: !globals.variant.name.startsWith('Throw It in a Hole') || globals.replay,
         listening: true,
     });
     globals.layers.UI.add(efficiencyNumberLabel);
@@ -1168,6 +1172,7 @@ const drawStatistics = () => {
         // "Easy" variants use the default color (off-white)
         // "Hard" variants use pink
         fill: (minEfficiency < 1.25 ? constants.LABEL_COLOR : '#ffb2b2'),
+        visible: !globals.variant.name.startsWith('Throw It in a Hole') || globals.replay,
         listening: true,
     });
     globals.layers.UI.add(efficiencyNumberLabelMinNeeded);
