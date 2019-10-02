9e0b10fd56e83de9431896c50aa8c4860f0f6e46
Zamiell <j.nesta@gmail.com>
2019-10-02 17:10:02 -0400

    updating run.sh

--- run.sh
+++ run.sh
@@ -5,12 +5,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd "$DIR/src"
-go install
-if [ $? -eq 0 ]; then
-    # The binary is called "src" by default, since the directory name is "src"
-    mv "$GOPATH/bin/src" "$GOPATH/bin/hanabi-live"
-    cd "$DIR"
-    "$GOPATH/bin/hanabi-live"
-else
-    echo "hanabi-live - Go compilation failed!"
-fi
+go run .
