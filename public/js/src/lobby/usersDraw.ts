aebc93440fa63ebbcc116456a0370bdf2913a593
Zamiell <j.nesta@gmail.com>
2020-03-22 13:24:53 -0400

    lobby users are now sorted alphabetically + purple color for self

--- public/js/src/lobby/usersDraw.ts
+++ public/js/src/lobby/usersDraw.ts
@@ -13,17 +13,39 @@ export default () => {
     // Clear all of the existing rows
     tbody.html('');
 
-    // Add all of the users
-    for (const [, user] of globals.userList) {
-        const row = $('<tr>');
+    // Make a mapping of user names to IDs
+    const usernameMapping: Map<string, number> = new Map();
+    for (const [id, user] of globals.userList) {
+        usernameMapping.set(user.name, id);
+    }
 
-        let name = `<a href="/scores/${user.name}" target="_blank" rel="noopener noreferrer">${user.name}</a>`;
-        if (user.name === globals.username) {
-            name = `<strong>${name}</strong>`;
+    // Make an alphabetical list of all of the usernames
+    const alphabeticalUsernames = Array.from(usernameMapping.keys()).sort();
+
+    // Add all of the users in alphabetical order
+    for (const username of alphabeticalUsernames) {
+        // Find the status of this user from the "userList" map
+        const id = usernameMapping.get(username);
+        if (!id) {
+            throw new Error(`Failed to get the ID for the username of "${username}".`);
         }
+        const user = globals.userList.get(id);
+        if (!user) {
+            throw new Error(`Failed to get the user for the ID of "${id}".`);
+        }
+        const status = user.status;
 
-        $('<td>').html(name).appendTo(row);
-        $('<td>').html(user.status).appendTo(row);
+        let nameColumn = `<a href="/scores/${username}" target="_blank" rel="noopener noreferrer">`;
+        if (username === globals.username) {
+            nameColumn += `<strong><span class="name-us">${username}</span></strong>`;
+        } else {
+            nameColumn += username;
+        }
+        nameColumn += '</a>';
+
+        const row = $('<tr>');
+        $('<td>').html(nameColumn).appendTo(row);
+        $('<td>').html(status).appendTo(row);
 
         row.appendTo(tbody);
     }
