5cb015a5c85826cbb26195fe3ab7cd6303fd1c1a
Zamiell <j.nesta@gmail.com>
2020-04-06 02:17:16 -0400

    adding clear empty tables command

--- src/httpLocalhost.go
+++ src/httpLocalhost.go
@@ -34,6 +34,7 @@ func httpLocalhostInit() {
 	httpRouter.GET("/ban/:username", httpUserAction)
 	httpRouter.GET("/mute/:username", httpUserAction)
 	httpRouter.GET("/uptime", httpUptime)
+	httpRouter.GET("/clearEmptyTables", httpClearEmptyTables)
 	httpRouter.GET("/debug", func(c *gin.Context) {
 		debug()
 		c.String(http.StatusOK, "success\n")
@@ -204,4 +205,42 @@ func httpUptime(c *gin.Context) {
 	msg += uptime + "\n"
 
 	c.String(http.StatusOK, msg)
+}
+
+// If calls to the database fail for whatever reason,
+// it is possible for tables to be created with no people in them
+// So we allow an administrator to clear them manually
+func httpClearEmptyTables(c *gin.Context) {
+	// First, make a slice of all of the map keys
+	tableIDs := make([]int, 0, len(tables))
+	for tableID := range tables {
+		tableIDs = append(tableIDs, tableID)
+	}
+
+	for _, tableID := range tableIDs {
+		var t *Table
+		if v, ok := tables[tableID]; !ok {
+			logger.Error("Failed to get the table with ID " + strconv.Itoa(tableID) + ".")
+			continue
+		} else {
+			t = v
+		}
+
+		if !t.Running {
+			// A table that has not started yet (e.g. pregame)
+			if len(t.Players) == 0 {
+				delete(tables, tableID)
+				notifyAllTableGone(t)
+			}
+		} else if t.Replay {
+			// A replay or shared replay
+			if len(t.Spectators) == 0 {
+				delete(tables, tableID)
+				notifyAllTableGone(t)
+			}
+		}
+		// (don't do anything for ongoing games)
+	}
+
+	c.String(http.StatusOK, "success\n")
 }
