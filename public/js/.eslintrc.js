1011cf614d1708a59f3e0651dce02ee037ba1b7d
Zamiell <j.nesta@gmail.com>
2020-04-06 15:12:15 -0400

    misc files didn't get spaces converted

--- public/js/.eslintrc.js
+++ public/js/.eslintrc.js
@@ -1,112 +1,112 @@
 module.exports = {
-    // The linter base is the Airbnb style guide, located here:
-    // https://github.com/airbnb/javascript
-    // The actual ESLint config is located here:
-    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js
-    extends: 'airbnb-typescript/base',
+  // The linter base is the Airbnb style guide, located here:
+  // https://github.com/airbnb/javascript
+  // The actual ESLint config is located here:
+  // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js
+  extends: 'airbnb-typescript/base',
 
-    env: {
-        browser: true,
-        jquery: true,
+  env: {
+    browser: true,
+    jquery: true,
+  },
+
+  // We need to specify some additional settings in order to make the linter work with TypeScript:
+  // https://medium.com/@myylow/how-to-keep-the-airbnb-eslint-config-when-moving-to-typescript-1abb26adb5c6
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: [ '@typescript-eslint' ],
+  settings: {
+    'import/extensions': ['.js', '.ts'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
     },
-
-    // We need to specify some additional settings in order to make the linter work with TypeScript:
-    // https://medium.com/@myylow/how-to-keep-the-airbnb-eslint-config-when-moving-to-typescript-1abb26adb5c6
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        project: './tsconfig.json',
+    'import/resolver': {
+      'node': {
+        'extensions': ['.js', '.ts'],
+      },
     },
-    plugins: [ '@typescript-eslint' ],
-    settings: {
-        'import/extensions': ['.js', '.ts'],
-        'import/parsers': {
-            '@typescript-eslint/parser': ['.ts']
-        },
-        'import/resolver': {
-            'node': {
-                'extensions': ['.js', '.ts']
-            }
-        }
-    },
+  },
 
-    // We modify the linting rules from the base for some specific things
-    // (listed in alphabetical order)
-    rules: {
-        // This allows code to be structured in a more logical order
-        // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/variables.js#L42
-        '@typescript-eslint/no-use-before-define': ['off'],
+  // We modify the linting rules from the base for some specific things
+  // (listed in alphabetical order)
+  rules: {
+    // This allows code to be structured in a more logical order
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/variables.js#L42
+    '@typescript-eslint/no-use-before-define': ['off'],
 
-        // The Hanabi codebase uses cyclical depedencies because
-        // various objects are attached to the global variables object,
-        // but methods of these objects also reference/change global variables
-        // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/imports.js#L236
-        'import/no-cycle': ['off'],
+    // The Hanabi codebase uses cyclical depedencies because
+    // various objects are attached to the global variables object,
+    // but methods of these objects also reference/change global variables
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/imports.js#L236
+    'import/no-cycle': ['off'],
 
-        // We want imports to be sorted alphabetically; this is not specified in the Airbnb config
-        // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/imports.js#L148
-        'import/order': ['error', {
-            groups: [['builtin', 'external', 'internal']],
-            alphabetize: { order: 'asc', caseInsensitive: true },
-        }],
+    // We want imports to be sorted alphabetically; this is not specified in the Airbnb config
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/imports.js#L148
+    'import/order': ['error', {
+      groups: [['builtin', 'external', 'internal']],
+      alphabetize: { order: 'asc', caseInsensitive: true },
+    }],
 
-        // Airbnb has "exceptAfterSingleLine" turned off by default
-        // A list of single-line variable declarations at the top of a class is common in TypeScript
-        // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js#L183
-        'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
+    // Airbnb has "exceptAfterSingleLine" turned off by default
+    // A list of single-line variable declarations at the top of a class is common in TypeScript
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js#L183
+    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
 
-        // The client makes use of some tasteful alerts
-        // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/best-practices.js#L59
-        'no-alert': ['off'],
+    // The client makes use of some tasteful alerts
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/best-practices.js#L59
+    'no-alert': ['off'],
 
-        // We need to output messages to the console for debugging
-        // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/errors.js#L27
-        'no-console': ['off'],
+    // We need to output messages to the console for debugging
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/errors.js#L27
+    'no-console': ['off'],
 
-        // We make use of constant while loops where appropriate
-        // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/errors.js#L30
-        'no-constant-condition': ['off'],
+    // We make use of constant while loops where appropriate
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/errors.js#L30
+    'no-constant-condition': ['off'],
 
-        // Proper use of continues can reduce indentation for long blocks of code
-        // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js#L279
-        'no-continue': ['off'],
+    // Proper use of continues can reduce indentation for long blocks of code
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js#L279
+    'no-continue': ['off'],
 
-        // Airbnb disallows mixing * and /, which is fairly nonsensical
-        // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js#L290
-        'no-mixed-operators': ['warn', { allowSamePrecedence: true }],
+    // Airbnb disallows mixing * and /, which is fairly nonsensical
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js#L290
+    'no-mixed-operators': ['warn', { allowSamePrecedence: true }],
 
-        // The Airbnb configuration allows 2 empty lines in a row, which is unneeded
-        // Additionally, the Airbnb configuration is bugged and
-        // allows a line at the beginning of the file
-        // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js#L316
-        'no-multiple-empty-lines': ['warn', { max: 1, maxBOF: 0, maxEOF: 0 }],
+    // The Airbnb configuration allows 2 empty lines in a row, which is unneeded
+    // Additionally, the Airbnb configuration is bugged and
+    // allows a line at the beginning of the file
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js#L316
+    'no-multiple-empty-lines': ['warn', { max: 1, maxBOF: 0, maxEOF: 0 }],
 
-        // We make use of parameter reassigning where appropriate
-        // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/best-practices.js#L190
-        'no-param-reassign': ['off'],
+    // We make use of parameter reassigning where appropriate
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/best-practices.js#L190
+    'no-param-reassign': ['off'],
 
-        // Airbnb disallows these because it can lead to errors with minified code;
-        // we don't have to worry about this in for loops though
-        // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js#L330
-        'no-plusplus': ['warn', { 'allowForLoopAfterthoughts': true }],
+    // Airbnb disallows these because it can lead to errors with minified code;
+    // we don't have to worry about this in for loops though
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js#L330
+    'no-plusplus': ['warn', { 'allowForLoopAfterthoughts': true }],
 
-        // Clean code can arise from for-of statements if used properly
-        // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js#L334
-        'no-restricted-syntax': ['off', 'ForOfStatement'],
+    // Clean code can arise from for-of statements if used properly
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js#L334
+    'no-restricted-syntax': ['off', 'ForOfStatement'],
 
-        // KineticJS's API has functions that are prefixed with an underscore
-        // (remove this once the code base is transitioned to Phaser)
-        // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js#L371
-        'no-underscore-dangle': ['off'],
+    // KineticJS's API has functions that are prefixed with an underscore
+    // (remove this once the code base is transitioned to Phaser)
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js#L371
+    'no-underscore-dangle': ['off'],
 
-        // Array destructuring can result in non-intuitive code
-        // Object destructuring is disgustingly verbose in TypeScript
-        // e.g. "const foo: string = bar.foo;" vs "const { foo }: { foo: string } = bar;"
-        // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/es6.js#L114
-        'prefer-destructuring': ['off'],
+    // Array destructuring can result in non-intuitive code
+    // Object destructuring is disgustingly verbose in TypeScript
+    // e.g. "const foo: string = bar.foo;" vs "const { foo }: { foo: string } = bar;"
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/es6.js#L114
+    'prefer-destructuring': ['off'],
 
-        // This allows for cleaner looking code as recommended here:
-        // https://blog.javascripting.com/2015/09/07/fine-tuning-airbnbs-eslint-config/
-        // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js#L448
-        'quote-props': ['warn', 'consistent-as-needed'],
-    },
+    // This allows for cleaner looking code as recommended here:
+    // https://blog.javascripting.com/2015/09/07/fine-tuning-airbnbs-eslint-config/
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js#L448
+    'quote-props': ['warn', 'consistent-as-needed'],
+  },
 };
--- public/js/webpack.config.ts
+++ public/js/webpack.config.ts
@@ -9,110 +9,110 @@ const epoch = new Date().getTime();
 
 // Read environment variables
 dotenv.config({
-    path: path.join(__dirname, '..', '..', '.env'),
+  path: path.join(__dirname, '..', '..', '.env'),
 });
 
 // Read the version
 const versionPath = path.join(__dirname, 'src', 'data', 'version.json');
 if (!fs.existsSync(versionPath)) {
-    throw new Error(`The version.json file does not exist at "${versionPath}".`);
+  throw new Error(`The version.json file does not exist at "${versionPath}".`);
 }
 const version = fs.readFileSync(versionPath).toString();
 
 // Delete old source maps
 const files = fs.readdirSync(__dirname);
 for (const file of files) {
-    const match = file.match(/main.min.js.\d+.map/g);
-    if (match) {
-        fs.unlinkSync(path.join(__dirname, file));
-    }
+  const match = file.match(/main.min.js.\d+.map/g);
+  if (match) {
+    fs.unlinkSync(path.join(__dirname, file));
+  }
 }
 
 const config: webpack.Configuration = {
-    // The entry file to bundle
-    entry: path.join(__dirname, 'src', 'main.ts'),
+  // The entry file to bundle
+  entry: path.join(__dirname, 'src', 'main.ts'),
 
-    // Where to put the bundled file
-    output: {
-        path: __dirname, // By default, Webpack will output the file to a "dist" subdirectory
-        filename: 'main.min.js',
-        // Chrome caches source maps and will not update them even after a hard-refresh
-        // Work around this by putting the epoch timestamp in the source map filename
-        sourceMapFilename: `main.min.js.${epoch}.map`,
-    },
+  // Where to put the bundled file
+  output: {
+    path: __dirname, // By default, Webpack will output the file to a "dist" subdirectory
+    filename: 'main.min.js',
+    // Chrome caches source maps and will not update them even after a hard-refresh
+    // Work around this by putting the epoch timestamp in the source map filename
+    sourceMapFilename: `main.min.js.${epoch}.map`,
+  },
 
-    resolve: {
-        extensions: ['.js', '.ts', '.json'],
-        symlinks: false, // Performance optimization
-    },
+  resolve: {
+    extensions: ['.js', '.ts', '.json'],
+    symlinks: false, // Performance optimization
+  },
 
-    // Webpack will display a warning unless we specify the mode
-    // Production mode minifies the resulting JavaScript, reducing the file size by a huge factor
-    // However, production mode takes a lot longer to pack than development mode,
-    // so only enable it on the real web server so that we can have speedy development
-    mode: (
-        process.platform === 'win32'
-        || process.platform === 'darwin' ? 'development' : 'production'
-    ),
+  // Webpack will display a warning unless we specify the mode
+  // Production mode minifies the resulting JavaScript, reducing the file size by a huge factor
+  // However, production mode takes a lot longer to pack than development mode,
+  // so only enable it on the real web server so that we can have speedy development
+  mode: (
+    process.platform === 'win32'
+    || process.platform === 'darwin' ? 'development' : 'production'
+  ),
 
-    // Loaders are transformations that are applied on the source code of a module
-    // https://webpack.js.org/concepts/loaders/
-    module: {
-        rules: [
-            // All files with a ".ts" extension (TypeScript files) will be handled by "ts-loader"
-            {
-                test: /\.ts$/,
-                include: path.join(__dirname, 'src'),
-                loader: 'ts-loader',
-            },
-        ],
-    },
-
-    plugins: [
-        // ProvidePlugin automatically loads modules instead of having to import them everywhere
-        // https://webpack.js.org/plugins/provide-plugin/
-        new webpack.ProvidePlugin({
-            // The Hanabi codebase and the Tooltipster library uses "$" to invoke jQuery
-            $: 'jquery',
-        }),
+  // Loaders are transformations that are applied on the source code of a module
+  // https://webpack.js.org/concepts/loaders/
+  module: {
+    rules: [
+      // All files with a ".ts" extension (TypeScript files) will be handled by "ts-loader"
+      {
+        test: /\.ts$/,
+        include: path.join(__dirname, 'src'),
+        loader: 'ts-loader',
+      },
     ],
+  },
 
-    // Ignore the warnings that recommend splitting up the codebase into separate bundles
-    stats: {
-        warningsFilter: [
-            'The following asset(s) exceed the recommended size limit',
-            'The following entrypoint(s) combined asset size exceeds the recommended limit',
-            'You can limit the size of your bundles by using import() or require.ensure to lazy load some parts of your application.',
-        ],
-    },
+  plugins: [
+    // ProvidePlugin automatically loads modules instead of having to import them everywhere
+    // https://webpack.js.org/plugins/provide-plugin/
+    new webpack.ProvidePlugin({
+      // The Hanabi codebase and the Tooltipster library uses "$" to invoke jQuery
+      $: 'jquery',
+    }),
+  ],
 
-    // Enable source maps for debugging purposes
-    // (this will show the line number of the real file in the browser console)
-    // Note that enabling source maps will not cause the end-user to download them unless they
-    // actually open the developer tools in their browser
-    // https://stackoverflow.com/questions/44315460/when-do-browsers-download-sourcemaps
-    devtool: 'source-map',
+  // Ignore the warnings that recommend splitting up the codebase into separate bundles
+  stats: {
+    warningsFilter: [
+      'The following asset(s) exceed the recommended size limit',
+      'The following entrypoint(s) combined asset size exceeds the recommended limit',
+      'You can limit the size of your bundles by using import() or require.ensure to lazy load some parts of your application.',
+    ],
+  },
+
+  // Enable source maps for debugging purposes
+  // (this will show the line number of the real file in the browser console)
+  // Note that enabling source maps will not cause the end-user to download them unless they
+  // actually open the developer tools in their browser
+  // https://stackoverflow.com/questions/44315460/when-do-browsers-download-sourcemaps
+  devtool: 'source-map',
 };
 
 if (
-    process.env.SENTRY_DSN !== ''
-    && process.env.TRAVIS !== 'true'
-    && process.platform !== 'win32'
-    && process.platform !== 'darwin'
+  process.env.SENTRY_DSN !== ''
+  && process.env.TRAVIS !== 'true'
+  && process.platform !== 'win32'
+  && process.platform !== 'darwin'
 ) {
-    if (typeof config.plugins === 'undefined') {
-        throw new Error('There are no existing plugins to append to.');
-    }
-    config.plugins.push(
-        // In order for Sentry to use the source maps, we must use their custom Webpack plugin
-        // This also uploads the packed file + source maps to Sentry
-        // https://docs.sentry.io/platforms/javascript/sourcemaps/
-        // (we don't want to upload anything in a development or testing environment)
-        new SentryWebpackPlugin({
-            include: path.join(__dirname, 'main.min.js'),
-            release: version,
-        }),
-    );
+  if (typeof config.plugins === 'undefined') {
+    throw new Error('There are no existing plugins to append to.');
+  }
+  config.plugins.push(
+    // In order for Sentry to use the source maps, we must use their custom Webpack plugin
+    // This also uploads the packed file + source maps to Sentry
+    // https://docs.sentry.io/platforms/javascript/sourcemaps/
+    // (we don't want to upload anything in a development or testing environment)
+    new SentryWebpackPlugin({
+      include: path.join(__dirname, 'main.min.js'),
+      release: version,
+    }),
+  );
 }
 
 export default config;
