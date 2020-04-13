db071f6978c7b5ddf94dab9634262477d9833050
Zamiell <j.nesta@gmail.com>
2020-04-13 22:06:16 +0000

    bugfix for install_dependencies.sh script

--- install/install_dependencies.sh
+++ install/install_dependencies.sh
@@ -7,6 +7,11 @@ set -x # Enable debugging
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# Get the name of the repository
+# https://stackoverflow.com/questions/23162299/how-to-get-the-last-part-of-dirname-in-bash/23162553
+REPO="$(dirname "$DIR")"
+REPO="$(basename "$REPO")"
+
 # Ensure that the ".env" file exists
 if [ ! -f "$DIR/../.env" ]; then
     cp "$DIR/../.env_template" "$DIR/../.env"
@@ -14,7 +19,7 @@ fi
 
 # Build the Golang files, which will automatically install the Golang dependencies
 cd "$DIR/../src"
-go build -o "$DIR/$REPO"
+go build -o "$DIR/../$REPO"
 
 # Install the JavaScript/TypeScript dependencies
 cd "$DIR/../public/js"
