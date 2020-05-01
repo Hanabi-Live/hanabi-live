91c92f850fd931dce0d91e9d7dfe0f883e025cdf
Zamiell <j.nesta@gmail.com>
2020-05-01 13:05:54 -0400

    colorblind mode clue borders are black

--- public/js/src/game/ui/HanabiCardInit.ts
+++ public/js/src/game/ui/HanabiCardInit.ts
@@ -45,7 +45,7 @@ export function border(this: HanabiCard) {
     height: CARD_H - 6,
     cornerRadius: 6,
     strokeWidth: 16,
-    stroke: 'orange',
+    stroke: globals.lobby.settings.colorblindMode ? 'black' : 'orange',
     visible: false,
     listening: false,
   });
