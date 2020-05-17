b102600fe996eae04dddccdd9dbc3c2b9a2503d5
Zamiell <j.nesta@gmail.com>
2020-05-17 01:04:29 -0400

    bugfix for friends being above our games

--- public/js/src/lobby/tablesDraw.ts
+++ public/js/src/lobby/tablesDraw.ts
@@ -23,25 +23,27 @@ const tablesDraw = () => {
   $('#lobby-games-table-container').show();
 
   // We want the tables to be drawn in a certain order:
-  // 1) Tables that you are currently in
-  // 2) Unstarted tables
-  // 3) Unstarted & password-protected tables
-  // 4) Ongoing tables
-  // 5) Shared replays
-  // Furthermore, we want tables with one or more of our friends to be drawn above all other tables
+  // 1) Tables that we are currently in
+  // 2) Tables our friends are currently in
+  // 3) Unstarted tables
+  // 4) Unstarted & password-protected tables
+  // 5) Ongoing tables
+  // 6) Shared replays
   let sortedTableIDs: number[] = [];
   for (const friends of [true, false]) {
     for (let i = 1; i <= 5; i++) {
       const tableIDsOfThisType: number[] = [];
       for (const [id, table] of globals.tableMap) {
-        const hasFriends = doesTableHaveFriends(table);
+        if (friends && i === 1 && table.joined && !table.sharedReplay) {
+          tableIDsOfThisType.push(id);
+        }
+
+        const hasFriends = tableHasFriends(table);
         if ((friends && !hasFriends) || (!friends && hasFriends)) {
           continue;
         }
 
-        if (i === 1 && table.joined && !table.sharedReplay) {
-          tableIDsOfThisType.push(id);
-        } else if (i === 2 && !table.running && !table.passwordProtected && !table.joined) {
+        if (i === 2 && !table.running && !table.passwordProtected && !table.joined) {
           tableIDsOfThisType.push(id);
         } else if (i === 3 && !table.running && table.passwordProtected && !table.joined) {
           tableIDsOfThisType.push(id);
@@ -194,7 +196,7 @@ const tableReattendButton = (table: Table) => () => {
   });
 };
 
-const doesTableHaveFriends = (table: Table) => {
+const tableHasFriends = (table: Table) => {
   for (const player of table.players) {
     if (globals.friends.includes(player)) {
       return true;
