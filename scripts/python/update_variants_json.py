bcbed60f5b50e99e094fa3b60090155e84a5b7a8
Zamiell <j.nesta@gmail.com>
2020-05-17 19:19:38 -0400

    adding special properties to variants.json

--- scripts/python/update_variants_json.py
+++ scripts/python/update_variants_json.py
@@ -10,10 +10,36 @@ with open(variants_json_path, 'r') as variants_file:
     variants = json.loads(variants_string)
 
 for key in variants.keys():
+    special_variant = False
     if '-Ones' in key:
+        special_variant = True
         variants[key]['specialRank'] = 1
     if '-Fives' in key:
+        special_variant = True
         variants[key]['specialRank'] = 5
+    if not special_variant:
+        continue
+
+    if 'Muddy-Rainbow-' in key:
+        variants[key]['specialAllClueColors'] = True
+        variants[key]['specialNoClueRanks'] = True
+    elif 'Light-Pink-' in key:
+        variants[key]['specialAllClueRanks'] = True
+        variants[key]['specialNoClueColors'] = True
+    elif 'Rainbow-' in key:
+        variants[key]['specialAllClueColors'] = True
+    elif 'Pink-' in key:
+        variants[key]['specialAllClueRanks'] = True
+    elif 'White-' in key:
+        variants[key]['specialNoClueColors'] = True
+    elif 'Brown-' in key:
+        variants[key]['specialNoClueRanks'] = True
+    elif 'Omni-' in key:
+        variants[key]['specialAllClueColors'] = True
+        variants[key]['specialAllClueRanks'] = True
+    elif 'Null-' in key:
+        variants[key]['specialNoClueColors'] = True
+        variants[key]['specialNoClueRanks'] = True
 
 with open(variants_json_path, 'w') as variants_file:
     json.dump(variants, variants_file)
