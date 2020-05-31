f708f645f5aab2deb21742e52349f7c2db7c2e58
Zamiell <j.nesta@gmail.com>
2020-05-31 00:37:41 -0400

    making webpack dev server port not hardcoded

--- webpack-dev-server.sh
+++ webpack-dev-server.sh
@@ -4,5 +4,11 @@
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# Import the WebPack port
+source "$DIR/.env"
+if [[ -z $WEBPACK_DEV_SERVER_PORT ]]; then
+  WEBPACK_DEV_SERVER_PORT=8080
+fi
+
 cd "$DIR/public/js"
-npx webpack-dev-server --port 8080
+npx webpack-dev-server --port $WEBPACK_DEV_SERVER_PORT
