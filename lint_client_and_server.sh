2a01740f6f54009a42fa413e98fe8f734b6d09c8
Zamiell <j.nesta@gmail.com>
2020-06-03 04:53:58 -0400

    bugfix for lint_client_and_server.sh

--- lint_client_and_server.sh
+++ lint_client_and_server.sh
@@ -6,5 +6,5 @@ set -e # Exit on any errors
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-"$DIR/lint_client.sh"
-"$DIR/lint_server.sh"
+"$DIR/client/lint_client.sh"
+"$DIR/server/lint_server.sh"
