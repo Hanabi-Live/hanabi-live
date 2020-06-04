ee25746705a9f3cdfee4b128855496cc7be9bff4
Roberto Sonnino <robertos@gmail.com>
2020-06-03 23:15:47 -0700

    Trust the server for clues, but log errors if the client state differs (#1183)
    Fix #1182 - It has a bug sometimes counting the clues and/or the cards left on deck

--- client/src/game/ui/hypothetical.ts
+++ client/src/game/ui/hypothetical.ts
@@ -19,7 +19,6 @@ import LayoutChild from './LayoutChild';
 import MsgClue from './MsgClue';
 import PlayerButton from './PlayerButton';
 import * as replay from './replay';
-import * as sideEffects from './sideEffects';
 import * as turn from './turn';
 
 export interface ActionReveal {
@@ -285,6 +284,7 @@ export const send = (hypoAction: ClientAction) => {
   // Status
   sendHypoAction({
     type: 'status',
+    clues: globals.variant.name.startsWith('Clue Starved') ? globals.clues * 2 : globals.clues,
     doubleDiscard: false,
     score: globals.score,
     maxScore: globals.maxScore,
@@ -301,11 +301,6 @@ export const send = (hypoAction: ClientAction) => {
     num: globals.turn,
     who: globals.currentPlayerIndex,
   });
-
-  // Side effects
-  // TODO: use real state changes instead of
-  // short-circuiting game logic.
-  sideEffects.changeClues(globals.clues);
 };
 
 export const sendHypoAction = (hypoAction: ActionIncludingHypothetical) => {
