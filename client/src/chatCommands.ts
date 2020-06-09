02f450ab5f531f43164f49427af5eb73304ad5f3
padiwik <24983586+padiwik@users.noreply.github.com>
2020-06-09 19:44:09 -0400

    Add /playerinfo command

--- client/src/chatCommands.ts
+++ client/src/chatCommands.ts
@@ -185,6 +185,33 @@ chatCommands.set('unfriend', (_room: string, args: string[]) => {
   });
 });
 
+// /playerinfo (username)
+const playerinfo = (_room: string, args: string[]) => {
+  let usernames: string[] = [];
+  if (args.length === 0) {
+    // If there are no arguments and we are at a table
+    // return stats for all the players
+    if (globals.tableID !== -1 && globals.ui) {
+      usernames = globals.ui.globals.playerNames;
+    } else {
+      // Otherwise, return stats for the caller
+      usernames = [globals.username];
+    }
+  } else {
+    // We can return the stats for a list of provided users separated by spaces
+    // since usernames cannot contain spaces
+    usernames = args;
+  }
+  for (const name of usernames) {
+    globals.conn!.send('chatPlayerInfo', {
+      name,
+    });
+  }
+};
+chatCommands.set('playerinfo', playerinfo);
+chatCommands.set('games', playerinfo);
+chatCommands.set('stats', playerinfo);
+
 // /version
 chatCommands.set('version', (room: string) => {
   chat.add({
