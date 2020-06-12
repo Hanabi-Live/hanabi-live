7595d140fd24a9c25011df486a0a3b4918b50f63
Roberto Sonnino <robertos@gmail.com>
2020-06-11 23:57:26 -0700

    Use describe() to group tests by method

--- client/src/game/rules/deck.test.ts
+++ client/src/game/rules/deck.test.ts
@@ -2,31 +2,33 @@ import { VARIANTS } from '../data/gameData';
 import { DEFAULT_VARIANT_NAME } from '../types/constants';
 import { totalCards } from './deck';
 
-test('totalCards for No Variant is 50', () => {
-  expect(totalCards(VARIANTS.get(DEFAULT_VARIANT_NAME)!)).toBe(50);
-});
+describe('totalCards', () => {
+  test('returns 50 for No Variant', () => {
+    expect(totalCards(VARIANTS.get(DEFAULT_VARIANT_NAME)!)).toBe(50);
+  });
 
-test.each`
-suits | cards
-${3}  | ${30}
-${4}  | ${40}
-${6}  | ${60}
-`('totalCards for $suits suits is $cards', ({ suits, cards }) => {
-  expect(totalCards(VARIANTS.get(`${suits} Suits`)!)).toBe(cards);
-});
+  test.each`
+  suits | cards
+  ${3}  | ${30}
+  ${4}  | ${40}
+  ${6}  | ${60}
+  `('returns $cards for $suits suits', ({ suits, cards }) => {
+    expect(totalCards(VARIANTS.get(`${suits} Suits`)!)).toBe(cards);
+  });
 
-test.each`
-suits | cards
-${5}  | ${45}
-${6}  | ${55}
-`('totalCards for Black ($suits suits) is $cards', ({ suits, cards }) => {
-  expect(totalCards(VARIANTS.get(`Black (${suits} Suits)`)!)).toBe(cards);
-});
+  test.each`
+  suits | cards
+  ${5}  | ${45}
+  ${6}  | ${55}
+  `('returns $cards for Black ($suits suits)', ({ suits, cards }) => {
+    expect(totalCards(VARIANTS.get(`Black (${suits} Suits)`)!)).toBe(cards);
+  });
 
-test.each`
-suits | cards
-${5}  | ${45}
-${6}  | ${54}
-`('totalCards for Up or Down ($suits suits) is $cards', ({ suits, cards }) => {
-  expect(totalCards(VARIANTS.get(`Up or Down (${suits} Suits)`)!)).toBe(cards);
+  test.each`
+  suits | cards
+  ${5}  | ${45}
+  ${6}  | ${54}
+  `('returns $cards for Up or Down ($suits suits)', ({ suits, cards }) => {
+    expect(totalCards(VARIANTS.get(`Up or Down (${suits} Suits)`)!)).toBe(cards);
+  });
 });
