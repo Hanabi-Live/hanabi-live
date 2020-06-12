bbbe31e4d504a1d597fa48aa1c33e4e9d02babd2
Roberto Sonnino <robertos@gmail.com>
2020-06-11 23:57:26 -0700

    Use describe() to group tests by method

--- client/src/game/rules/clues.test.ts
+++ client/src/game/rules/clues.test.ts
@@ -2,18 +2,20 @@ import { VARIANTS } from '../data/gameData';
 import { DEFAULT_VARIANT_NAME, MAX_CLUE_NUM } from '../types/constants';
 import { gainClue } from './clues';
 
-test.each([...Array(8).keys()])('gainClue adds a clue when there are %i clues', (n) => {
-  expect(gainClue(VARIANTS.get(DEFAULT_VARIANT_NAME)!, n)).toBe(n + 1);
-});
+describe('gainClue', () => {
+  test.each([...Array(8).keys()])('adds a clue when there are %i clues', (n) => {
+    expect(gainClue(VARIANTS.get(DEFAULT_VARIANT_NAME)!, n)).toBe(n + 1);
+  });
 
-test('gainClue does not add clues when maxed out', () => {
-  expect(gainClue(VARIANTS.get(DEFAULT_VARIANT_NAME)!, MAX_CLUE_NUM)).toBe(MAX_CLUE_NUM);
-});
+  test('does not add clues when maxed out', () => {
+    expect(gainClue(VARIANTS.get(DEFAULT_VARIANT_NAME)!, MAX_CLUE_NUM)).toBe(MAX_CLUE_NUM);
+  });
 
-test.each([...Array(8).keys()])('gainClue adds half clue for Clue Starved variants', (n) => {
-  expect(gainClue(VARIANTS.get('Clue Starved (5 Suits)')!, n)).toBeCloseTo(n + 0.5);
-});
+  test.each([...Array(8).keys()])('adds half clue for Clue Starved variants', (n) => {
+    expect(gainClue(VARIANTS.get('Clue Starved (5 Suits)')!, n)).toBeCloseTo(n + 0.5);
+  });
 
-test('gainClue does not add a clue when maxed out for Clue Starved variants', () => {
-  expect(gainClue(VARIANTS.get('Clue Starved (5 Suits)')!, MAX_CLUE_NUM)).toBe(MAX_CLUE_NUM);
+  test('does not add a clue when maxed out for Clue Starved variants', () => {
+    expect(gainClue(VARIANTS.get('Clue Starved (5 Suits)')!, MAX_CLUE_NUM)).toBe(MAX_CLUE_NUM);
+  });
 });
