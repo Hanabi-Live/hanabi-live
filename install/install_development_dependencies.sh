6fc8871a7fead9d6e118f2a6b872fa9fc80eb786
Zamiell <j.nesta@gmail.com>
2020-06-16 15:05:13 -0400

    making golang linter version into a variable

--- install/install_development_dependencies.sh
+++ install/install_development_dependencies.sh
@@ -3,6 +3,9 @@
 set -e # Exit on any errors
 set -x # Enable debugging
 
+# Import the linter version
+source "$DIR/../server/.golangci-lint.version"
+
 # Get the directory of this script
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -14,7 +17,7 @@ code --install-extension "dbaeumer.vscode-eslint" # For TypeScript
 # Install the Golang linter
 # (it is not recommended to install this with "go get")
 # https://github.com/golangci/golangci-lint#install
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.27.0
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin $GOLANGCI_LINT_VERSION
 
 # Install the TypeScript linter
 # https://www.npmjs.com/package/eslint-config-airbnb
