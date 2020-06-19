14c03f371b403599d7d9f498e3f063a0976d4c45
Zamiel <j.nesta@gmail.com>
2020-06-19 20:46:52 +0000

    removing dupe script

--- version_client.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get the directory of this script
-# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-cat "$DIR/public/js/bundles/version.txt"
