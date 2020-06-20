2ede4e80e604f92b7ae1bb324c9222425f331033
Zamiell <j.nesta@gmail.com>
2020-06-20 19:56:39 -0400

    adding efficiency test

--- client/src/game/reducers/gameStateReducer.test.ts
+++ client/src/game/reducers/gameStateReducer.test.ts
@@ -12,15 +12,6 @@ if (defaultVariant === undefined) {
   throw new Error('Unable to find the default variant in the "VARIANTS" map.');
 }
 
-const testClue: ActionClue = {
-  type: 'clue',
-  clue: { type: ClueType.Rank, value: 5 },
-  giver: 1,
-  target: 0,
-  turn: 2,
-  list: [],
-};
-
 const testText: ActionText = {
   type: 'text',
   text: 'testing',
@@ -41,6 +32,18 @@ describe('stateReducer', () => {
     test('adds the clue to the list of clues', () => {
       const state = initialGameState(defaultVariant, 3);
 
+      const testClue: ActionClue = {
+        type: 'clue',
+        clue: {
+          type: ClueType.Rank,
+          value: 5,
+        },
+        giver: 1,
+        target: 0,
+        turn: 2,
+        list: [],
+      };
+
       const newState = gameStateReducer(state, testClue);
       expect(newState.clues.length).toBe(state.clues.length + 1);
       expect(newState.clues[0].giver).toBe(testClue.giver);
@@ -48,6 +51,40 @@ describe('stateReducer', () => {
       expect(newState.clues[0].turn).toBe(testClue.turn);
       expect(newState.clues[0].type).toBe(testClue.clue.type);
       expect(newState.clues[0].value).toBe(testClue.clue.value);
+    });
+
+    test('makes the efficiency go up', () => {
+      // Arrange
+      let state = initialGameState(defaultVariant, 3);
+
+      // Act
+      for (let i = 0; i < 3; i++) {
+        const draw: ActionDraw = {
+          type: 'draw',
+          rank: i + 1,
+          suit: 0,
+          who: 0,
+          order: i,
+        };
+        state = gameStateReducer(state, draw);
+      }
+
+      // Give a 3-for-1 clue to the 3 red cards
+      const redClue: ActionClue = {
+        type: 'clue',
+        clue: {
+          type: ClueType.Color,
+          value: 0,
+        },
+        giver: 1,
+        target: 0,
+        turn: 0,
+        list: [0, 1, 2],
+      };
+      state = gameStateReducer(state, redClue);
+
+      // Assert
+      expect(state.stats.efficiency).toBe(3);
     });
   });
 
@@ -69,14 +106,21 @@ describe('stateReducer', () => {
 
         // Act
         const draw: ActionDraw = {
-          type: 'draw', rank: 1, suit: 0, who: 0, order: 0,
+          type: 'draw',
+          rank: 1,
+          suit: 0,
+          who: 0,
+          order: 0,
         };
         state = gameStateReducer(state, draw);
 
         const blindPlay: ActionPlay = {
           type: 'play',
           which: {
-            index: 0, suit: 0, rank: 1, order: 0,
+            index: 0,
+            suit: 0,
+            rank: 1,
+            order: 0,
           },
         };
         state = gameStateReducer(state, blindPlay);
