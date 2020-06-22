49386b64de4a9aed37f853a8ce242b3392c8e1bc
Zamiell <j.nesta@gmail.com>
2020-06-22 19:27:10 -0400

    bugfix for restored tables no initializing goroutines

--- server/src/restart.go
+++ server/src/restart.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"path"
 	"runtime"
-	"strconv"
 )
 
 // We want to record all of the ongoing games to a flat file on the disk
@@ -61,67 +57,4 @@ func restart() {
 
 	// Block until the process is killed so that no more moves can be submitted
 	select {}
-}
-
-func serializeTables() bool {
-	for _, t := range tables {
-		logger.Info("Serializing table:", t.ID)
-
-		// Only serialize ongoing games
-		if !t.Running || t.Replay {
-			logger.Info("Skipping due to it being unstarted or a replay.")
-			continue
-		}
-
-		// Force all of the spectators to leave, if any
-		for _, sp := range t.Spectators {
-			s := sp.Session
-			if s == nil {
-				// A spectator's session should never be nil
-				// They might be in the process of reconnecting,
-				// so make a fake session that will represent them
-				s = newFakeSession(sp.ID, sp.Name)
-				logger.Info("Created a new fake session in the \"serializeTables()\" function.")
-			} else {
-				// Boot them from the game
-				s.Emit("boot", nil)
-			}
-			commandTableUnattend(s, &CommandData{
-				TableID: t.ID,
-			})
-		}
-		if len(t.Spectators) > 0 {
-			t.Spectators = make([]*Spectator, 0)
-		}
-		if len(t.DisconSpectators) > 0 {
-			t.DisconSpectators = make(map[int]struct{})
-		}
-
-		// Set all the player sessions to nil, since it is not necessary to serialize those
-		for _, p := range t.Players {
-			p.Session = nil
-			p.Present = false
-		}
-
-		// "t.Game.Table", "t.Game.Options", and "t.Game.ExtraOptions" are circular references;
-		// we do not have to unset them because we have specified `json:"-"` on their fields,
-		// so the JSON encoder will ignore them
-
-		var tableJSON []byte
-		if v, err := json.Marshal(t); err != nil {
-			logger.Error("Failed to marshal table "+strconv.Itoa(t.ID)+":", err)
-			return false
-		} else {
-			tableJSON = v
-		}
-
-		tablePath := path.Join(tablesPath, strconv.Itoa(t.ID)+".json")
-		if err := ioutil.WriteFile(tablePath, tableJSON, 0600); err != nil {
-			logger.Error("Failed to write the table "+strconv.Itoa(t.ID)+" to "+
-				"\""+tablePath+"\":", err)
-			return false
-		}
-	}
-
-	return true
 }
--- server/src/serialize_tables.go
+++ server/src/serialize_tables.go
@@ -12,6 +12,70 @@ import (
 	"github.com/mitchellh/mapstructure"
 )
 
+// serializeTables saves any ongoing tables to disk as JSON files so that they can be restored later
+func serializeTables() bool {
+	for _, t := range tables {
+		logger.Info("Serializing table:", t.ID)
+
+		// Only serialize ongoing games
+		if !t.Running || t.Replay {
+			logger.Info("Skipping due to it being unstarted or a replay.")
+			continue
+		}
+
+		// Force all of the spectators to leave, if any
+		for _, sp := range t.Spectators {
+			s := sp.Session
+			if s == nil {
+				// A spectator's session should never be nil
+				// They might be in the process of reconnecting,
+				// so make a fake session that will represent them
+				s = newFakeSession(sp.ID, sp.Name)
+				logger.Info("Created a new fake session in the \"serializeTables()\" function.")
+			} else {
+				// Boot them from the game
+				s.Emit("boot", nil)
+			}
+			commandTableUnattend(s, &CommandData{
+				TableID: t.ID,
+			})
+		}
+		if len(t.Spectators) > 0 {
+			t.Spectators = make([]*Spectator, 0)
+		}
+		if len(t.DisconSpectators) > 0 {
+			t.DisconSpectators = make(map[int]struct{})
+		}
+
+		// Set all the player sessions to nil, since it is not necessary to serialize those
+		for _, p := range t.Players {
+			p.Session = nil
+			p.Present = false
+		}
+
+		// "t.Game.Table", "t.Game.Options", and "t.Game.ExtraOptions" are circular references;
+		// we do not have to unset them because we have specified `json:"-"` on their fields,
+		// so the JSON encoder will ignore them
+
+		var tableJSON []byte
+		if v, err := json.Marshal(t); err != nil {
+			logger.Error("Failed to marshal table "+strconv.Itoa(t.ID)+":", err)
+			return false
+		} else {
+			tableJSON = v
+		}
+
+		tablePath := path.Join(tablesPath, strconv.Itoa(t.ID)+".json")
+		if err := ioutil.WriteFile(tablePath, tableJSON, 0600); err != nil {
+			logger.Error("Failed to write the table "+strconv.Itoa(t.ID)+" to "+
+				"\""+tablePath+"\":", err)
+			return false
+		}
+	}
+
+	return true
+}
+
 // restoreTables recreates tables that were ongoing at the time of the last server restart
 // Tables were serialized to flat files in the "tablesPath" directory
 func restoreTables() {
@@ -68,10 +132,18 @@ func restoreTables() {
 			// (we don't have to bother converting any other actions)
 		}
 
-		// If this is a timed game, give the current player some additional seconds
-		// to make up for the fact that they are forced to refresh
+		// Restored tables will never be automatically terminated due to idleness because the
+		// "CheckIdle()" function was never initiated; manually do this
+		go t.CheckIdle()
+
 		if g.Options.Timed {
+			// Give the current player some additional seconds to make up for the fact that they are
+			// forced to refresh
 			g.Players[g.ActivePlayer].Time += 20 * time.Second
+
+			// Players will never run out of time on restored tables because the "CheckTimer()"
+			// function was never initiated; manually do this
+			go g.CheckTimer(g.Turn, g.PauseCount, g.Players[g.ActivePlayer])
 		}
 
 		tables[t.ID] = t
