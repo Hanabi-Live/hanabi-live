beaa3820279376bbbe967d578b785cd17112f549
Zamiell <j.nesta@gmail.com>
2020-06-24 14:58:19 -0400

    better localhost terminate

--- server/src/http_localhost_terminate.go
+++ server/src/http_localhost_terminate.go
@@ -11,26 +11,43 @@ func httpLocalhostTerminate(c *gin.Context) {
 	// Local variables
 	w := c.Writer
 
-	// Validate the table ID
-	tableIDString := c.PostForm("tableID")
-	if tableIDString == "" {
-		http.Error(w, "Error: You must specify a table ID.", http.StatusBadRequest)
+	// Validate the table name / table ID
+	tableNameOrID := c.PostForm("tableID")
+	if tableNameOrID == "" {
+		http.Error(w, "Error: You must specify a table name or a table ID.", http.StatusBadRequest)
 		return
 	}
+
+	searchingByName := false
 	var tableID int
-	if v, err := strconv.Atoi(tableIDString); err != nil {
-		http.Error(w, "Error: That is not a valid number.", http.StatusBadRequest)
-		return
+	if v, err := strconv.Atoi(tableNameOrID); err != nil {
+		searchingByName = true
 	} else {
 		tableID = v
 	}
 
-	// Check to see if this table ID exists
-	if t, ok := tables[tableID]; !ok {
-		c.String(http.StatusOK, "Table \""+strconv.Itoa(tableID)+"\" does not exist.\n")
-		return
+	// Get the corresponding table
+	var matchingTable *Table
+	if searchingByName {
+		for _, t := range tables {
+			if t.Name == tableNameOrID {
+				matchingTable = t
+				break
+			}
+		}
+		if matchingTable == nil {
+			c.String(http.StatusOK, "Table \""+tableNameOrID+"\" does not exist.\n")
+			return
+		}
 	} else {
-		// Terminate it
-		terminate(t, "Hanabi Live", -1)
+		if v, ok := tables[tableID]; !ok {
+			c.String(http.StatusOK, "Table \""+strconv.Itoa(tableID)+"\" does not exist.\n")
+			return
+		} else {
+			matchingTable = v
+		}
 	}
+
+	// Terminate it
+	terminate(matchingTable, "Hanabi Live", -1)
 }
