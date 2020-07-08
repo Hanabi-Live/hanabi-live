7b0ee0e4d955d8b3ad91242e863db818f2bf254c
Zamiell <j.nesta@gmail.com>
2020-07-08 17:19:46 -0400

    making server backwards compatibile

--- server/src/card_identity.go
+++ server/src/card_identity.go
@@ -1,6 +1,9 @@
 package main
 
 type CardIdentity struct {
-	SuitIndex int `json:"suitIndex"`
+	// When reading data from a client, the server will accept either "suitIndex" or "suit" for
+	// backwards compatibility
+	// When sending data to a client, it will prefer the first tag e.g. "suitIndex"
+	SuitIndex int `json:"suitIndex" json:"suit"` // nolint: staticcheck
 	Rank      int `json:"rank"`
 }
