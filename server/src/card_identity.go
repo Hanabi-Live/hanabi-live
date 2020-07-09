6e6a23270a71620bf06dbff6d1dc17d69575e59a
Zamiell <j.nesta@gmail.com>
2020-07-09 03:47:17 -0400

    Revert "making server backwards compatibile"
    This reverts commit 7b0ee0e4d955d8b3ad91242e863db818f2bf254c.

--- server/src/card_identity.go
+++ server/src/card_identity.go
@@ -1,9 +1,6 @@
 package main
 
 type CardIdentity struct {
-	// When reading data from a client, the server will accept either "suitIndex" or "suit" for
-	// backwards compatibility
-	// When sending data to a client, it will prefer the first tag e.g. "suitIndex"
-	SuitIndex int `json:"suitIndex" json:"suit"` // nolint: staticcheck
+	SuitIndex int `json:"suitIndex"`
 	Rank      int `json:"rank"`
 }
