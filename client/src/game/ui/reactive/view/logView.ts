ee7d03b86c1f2bc2e494a020c006a3557b4d7648
Zamiell <j.nesta@gmail.com>
2020-07-11 02:23:30 -0400

    functions instead of comments

--- client/src/game/ui/reactive/view/logView.ts
+++ client/src/game/ui/reactive/view/logView.ts
@@ -4,7 +4,11 @@ import { LogEntry } from '../../../types/GameState';
 import globals from '../../globals';
 
 export function onLogChanged(log: readonly LogEntry[]) {
-  // Update the action log
+  updateActionLog(log);
+  updateFullActionLog(log);
+}
+
+const updateActionLog = (log: readonly LogEntry[]) => {
   const actionLog = globals.elements.actionLog!;
   const startingIndex = Math.max(0, log.length - actionLog.maxLines);
   for (let i = 0; i < actionLog.maxLines; i++) {
@@ -15,11 +19,13 @@ export function onLogChanged(log: readonly LogEntry[]) {
   }
   actionLog.refreshText();
 
-  // Update the full action log
+  globals.layers.UI.batchDraw();
+};
+
+const updateFullActionLog = (log: readonly LogEntry[]) => {
   const fullActionLog = globals.elements.fullActionLog!;
   fullActionLog.reset();
   log.forEach((line) => fullActionLog.addMessage(line.turn, line.text));
 
-  globals.layers.UI.batchDraw();
   globals.layers.UI2.batchDraw();
-}
+};
