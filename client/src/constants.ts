c32d062f6db5ea537c9a4354b568d169d0b71aef
Roberto Sonnino <robertos@gmail.com>
2020-07-14 18:31:22 -0700

    Change rank pips to look like numbers (#1461)
    * Replay the game as spectator when it ends
    
    * PR feedback
    
    * Change rank pips to look like numbers

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -6,20 +6,22 @@ export const CARD_H = 406;
 export const PLAY_AREA_PADDING = 1.15;
 export const HAND_PADDING = 1.05;
 export const HAND_BASE_SCALE = 0.4;
-// This is a temporary scale only to be used with phaser until dynamic scaling is implemented
-export const PLAY_AREA_BASE_SCALE = 0.4;
 export const CARD_FADE = 0.6;
 export const STRIKE_FADE = 0.125;
 
-// Other miscellaneous constants for the UI
+// Colors
 export const LABEL_COLOR = '#d8d5ef'; // Off-white
-export const TOOLTIP_DELAY = 500; // In milliseconds
 export const ARROW_COLOR = {
   DEFAULT: '#ffffff', // White
   RETOUCHED: '#737373', // Dark gray
   HIGHLIGHT: '#ffdf00', // Yellow
 };
-export const FADE_TIME = 350; // In milliseconds
+export const CLUED_COLOR = 'orange';
+export const CHOP_MOVE_COLOR = '#fffce6'; // White with a yellow tint
+export const FINESSE_COLOR = 'aqua';
 
+// Time constants
+export const FADE_TIME = 350; // In milliseconds
+export const TOOLTIP_DELAY = 500; // In milliseconds
 export const PREPLAY_DELAY = 50; // In milliseconds
 export const SHUTDOWN_TIMEOUT = 30; // In minutes
