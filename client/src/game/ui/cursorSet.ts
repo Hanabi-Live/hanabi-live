ba64fbba49a6db4243397aeb17346ad5ec543413
Zamiell <j.nesta@gmail.com>
2020-07-16 12:09:44 -0400

    making cursors smaller + removing default themed cursor

--- client/src/game/ui/cursorSet.ts
+++ client/src/game/ui/cursorSet.ts
@@ -18,7 +18,7 @@ export default function cursorSet(cursorType: CursorType) {
   currentCursorType = cursorType;
   const url = `/public/img/cursors/${cursorType}.png`;
   const cursorValue = `url('${url}'), auto`;
-  globals.stage.container().style.cursor = cursorValue;
+  globals.stage.container().style.cursor = cursorType === 'default' ? 'auto' : cursorValue;
 
   // If the Chrome development tools are open, then the cursor may not update properly
   // https://stackoverflow.com/questions/37462132/update-mouse-cursor-without-moving-mouse-with-changed-css-cursor-property
