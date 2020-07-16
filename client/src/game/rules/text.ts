cc27effa3d9adb5dc0e187c2ada194831aa91e1e
Zamiell <j.nesta@gmail.com>
2020-07-16 12:44:03 -0400

    bugfix for cow & pig

--- client/src/game/rules/text.ts
+++ client/src/game/rules/text.ts
@@ -41,17 +41,6 @@ export function clue(action: ActionClue, targetHand: number[], metadata: GameMet
     || variantRules.isDuck(variant)
     || characterName === 'Quacker'
   ) {
-    // Create a list of slot numbers that correspond to the cards touched
-    const slots: number[] = [];
-    for (const order of action.list) {
-      const slot = handRules.cardSlot(order, targetHand);
-      if (slot === null) {
-        throw new Error(`Failed to get the slot for card ${order}.`);
-      }
-      slots.push(slot);
-    }
-    slots.sort();
-
     let actionName = 'clues';
     if (variantRules.isCowAndPig(variant)) {
       if (action.clue.type === ClueType.Color) {
@@ -67,9 +56,26 @@ export function clue(action: ActionClue, targetHand: number[], metadata: GameMet
     if (!target.endsWith('s')) {
       target += 's';
     }
+
+    // Create a list of slot numbers that correspond to the cards touched
+    const slots: number[] = [];
+    for (const order of action.list) {
+      const slot = handRules.cardSlot(order, targetHand);
+      if (slot === null) {
+        throw new Error(`Failed to get the slot for card ${order}.`);
+      }
+      slots.push(slot);
+    }
+    slots.sort();
+
+    let slotWord = 'slot';
+    if (slots.length !== 1) {
+      slotWord += 's';
+    }
+
     const slotsText = slots.join('/');
 
-    return `${giver} ${actionName} at ${target} ${slotsText}'`;
+    return `${giver} ${actionName} at ${target} ${slotWord} ${slotsText}`;
   }
 
   // Handle the default case of a normal clue
