bc82498af827afe8dda6e0876a03b94b299eb42a
Zamiell <j.nesta@gmail.com>
2020-07-20 02:37:51 -0400

    removing useless code now that the tooltip automatically closes

--- client/src/game/ui/notes.ts
+++ client/src/game/ui/notes.ts
@@ -315,19 +315,6 @@ export const openEditTooltip = (card: HanabiCard) => {
     return;
   }
 
-  if (globals.editingNote !== null) {
-    // Close any existing note tooltips
-    const tooltip = $(`#tooltip-card-${globals.editingNote}`);
-    tooltip.tooltipster('close');
-
-    // If we are right clicking the card that we were already editing,
-    // then just close the existing tooltip and don't do anything else
-    if (card.state.order === globals.editingNote) {
-      globals.editingNote = null;
-      return;
-    }
-  }
-
   show(card);
 
   globals.editingNote = card.state.order;
