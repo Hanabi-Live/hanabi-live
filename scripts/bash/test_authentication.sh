b9030385d6543846d7321cc46390a06ae24dcdb1
Zamiel <j.nesta@gmail.com>
2020-07-21 05:04:35 +0000

    better script

--- scripts/bash/test_authentication.sh
+++ scripts/bash/test_authentication.sh
@@ -8,10 +8,23 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$DIR/../../.env"
 
 URL_PREFIX="https"
+WS_PREFIX="wss"
 if [[ -z $TLS_CERT_FILE ]]; then
   URL_PREFIX="http"
+  WS_PREFIX="ws"
 fi
 URL="$URL_PREFIX://$DOMAIN:$PORT/login"
+WEBSOCKET_URL="$WS_PREFIX://$DOMAIN:$PORT/ws"
+WEBSOCKET_URL="$WS_PREFIX://$DOMAIN:$PORT/ws"
 
-curl --verbose --data "username=test&password=test&version=bot" "$URL"
+echo "Getting a cookie..."
+echo curl --verbose --dump-header "cookie.txt" --data "username=test&password=test&version=bot" "$URL"
+curl --verbose --dump-header "cookie.txt" --data "username=test&password=test&version=bot" "$URL"
 echo
+
+COOKIE=$(grep -i 'Set-Cookie' cookie.txt | awk '/hanabi.sid=/{print $2}')
+HTTP_HEADER="Cookie: $COOKIE"
+
+echo "Connecting to the WebSocket server..."
+echo wscat --header "$HTTP_HEADER" --connect "$WEBSOCKET_URL"
+wscat --header "$HTTP_HEADER" --connect "$WEBSOCKET_URL"
