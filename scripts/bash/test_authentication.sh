496b1da9eadb0c70f1a99bdc0687a1a6aaee1d0c
Zamiell <j.nesta@gmail.com>
2020-07-21 01:19:44 -0400

    better script

--- scripts/bash/test_authentication.sh
+++ scripts/bash/test_authentication.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Requirements:
+# npm install -g wscat
+
+# Configuration
+HANABI_USERNAME="test"
+HANABI_PASSWORD="test"
+
 # Get the directory of this script
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -18,13 +25,14 @@ WEBSOCKET_URL="$WS_PREFIX://$DOMAIN:$PORT/ws"
 WEBSOCKET_URL="$WS_PREFIX://$DOMAIN:$PORT/ws"
 
 echo "Getting a cookie..."
-echo curl --verbose --dump-header "cookie.txt" --data "username=test&password=test&version=bot" "$URL"
-curl --verbose --dump-header "cookie.txt" --data "username=test&password=test&version=bot" "$URL"
-echo
-
-COOKIE=$(grep -i 'Set-Cookie' cookie.txt | awk '/hanabi.sid=/{print $2}')
+COOKIE_FILENAME="cookie.txt"
+echo curl --verbose --dump-header "$COOKIE_FILENAME" --data "username=$HANABI_USERNAME&password=$HANABI_PASSWORD&version=bot" "$URL"
+curl --verbose --dump-header "$COOKIE_FILENAME" --data "username=$HANABI_USERNAME&password=$HANABI_PASSWORD&version=bot" "$URL"
+COOKIE=$(grep -i 'Set-Cookie' "$COOKIE_FILENAME" | awk '/hanabi.sid=/{print $2}')
 HTTP_HEADER="Cookie: $COOKIE"
+rm -f "$COOKIE_FILENAME"
 
+echo
 echo "Connecting to the WebSocket server..."
 echo wscat --header "$HTTP_HEADER" --connect "$WEBSOCKET_URL"
 wscat --header "$HTTP_HEADER" --connect "$WEBSOCKET_URL"
