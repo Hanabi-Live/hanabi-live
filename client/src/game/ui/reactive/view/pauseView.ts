42627bdbbe868f17043907e91d97f5fd58b9cb85
Zamiel <5511220+Zamiell@users.noreply.github.com>
2020-08-24 14:21:38 -0400

    potential bugfix for non-init ui elemented

--- client/src/game/ui/reactive/view/pauseView.ts
+++ client/src/game/ui/reactive/view/pauseView.ts
@@ -6,10 +6,10 @@ import isOurTurn from '../../isOurTurn';
 
 export const onChanged = (pause: PauseState) => {
   const stageFadeOpacity = pause.active ? 0.8 : 0.3;
-  globals.elements.stageFade!.opacity(stageFadeOpacity);
-  globals.elements.stageFade!.visible(pause.active);
+  globals.elements.stageFade?.opacity(stageFadeOpacity);
+  globals.elements.stageFade?.visible(pause.active);
 
-  globals.elements.pauseArea!.visible(pause.active);
+  globals.elements.pauseArea?.visible(pause.active);
 
   // The timer elements may not be initialized under certain conditions (e.g. an untimed game)
   globals.elements.timer1?.visible(!pause.active && globals.state.playing);
@@ -17,14 +17,14 @@ export const onChanged = (pause: PauseState) => {
   globals.elements.timer1Circle?.visible(pause.queued);
 
   if (pause.active) {
-    globals.elements.pauseText!.text(`by: ${globals.metadata.playerNames[pause.playerIndex]}`);
+    globals.elements.pauseText?.text(`by: ${globals.metadata.playerNames[pause.playerIndex]}`);
 
     if (globals.state.playing) {
-      globals.elements.pauseButton!.setEnabled(true);
-      globals.elements.pauseButton!.opacity(1);
+      globals.elements.pauseButton?.setEnabled(true);
+      globals.elements.pauseButton?.opacity(1);
     } else {
-      globals.elements.pauseButton!.setEnabled(false);
-      globals.elements.pauseButton!.opacity(0.2);
+      globals.elements.pauseButton?.setEnabled(false);
+      globals.elements.pauseButton?.opacity(0.2);
     }
   }
 
