6bdecbc0154ca8015580bd5aa646e58b05105f89
Zamiel <5511220+Zamiell@users.noreply.github.com>
2020-09-01 13:37:21 -0400

    restoring stats updating at the end of a game

--- server/src/game_end.go
+++ server/src/game_end.go
@@ -252,7 +252,7 @@ func (g *Game) WriteDatabase() error {
 	}
 
 	// We also need to update stats in the database, but that can be done in the background
-	// go g.WriteDatabaseStats() // Commented out while we do a mass update
+	go g.WriteDatabaseStats()
 
 	logger.Info("Finished core database actions for table " + strconv.FormatUint(t.ID, 10) +
 		" (to database ID " + strconv.Itoa(t.ExtraOptions.DatabaseID) + ").")
