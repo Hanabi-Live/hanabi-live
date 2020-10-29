5698cb31dc7e7e45aafc24a79de1b992eebb4a06
Zamiell <5511220+Zamiell@users.noreply.github.com>
2020-10-29 17:34:49 -0400

    linter no longer needs to be separately installed

--- lint_client.sh
+++ lint_client.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -e # Exit on any errors
+
 # Get the directory of this script
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd "$DIR"
-npm install
-npx install-peerdeps --dev eslint-config-airbnb-base
+npm run lint
+echo "Success!"
