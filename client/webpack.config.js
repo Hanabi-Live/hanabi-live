b5d47764580d33d5ad188ad7806df28416f6e263
Zamiell <5511220+Zamiell@users.noreply.github.com>
2020-10-29 21:46:10 -0400

    bugfix for linter

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 // It is possible for the Webpack configuration to be written in TypeScript,
