f7ce02773b204705d27536bf9f3f1b93a35b0bab
Zamiell <5511220+Zamiell@users.noreply.github.com>
2020-11-28 09:15:26 -0500

    bugfix for set_version.sh

--- client/set_version.sh
+++ client/set_version.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Get the directory of this script
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # Set the version number in the "version.json" file
 # (which is equal to the number of commits in the git repository)
 # This is "baked" into the JavaScript bundle and self-reported when connecting to the server so that
