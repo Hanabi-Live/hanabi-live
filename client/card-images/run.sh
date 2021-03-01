a688d85b472b9e330b8da60adc7abcaa2a9b048f
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-03-01 15:03:03 -0500

    better run script

--- client/card-images/run.sh
+++ client/card-images/run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+set -e # Exit on any errors
+
 # Get the directory of this script
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 node "$DIR/dist/client/card-images/createAllCards.js"
+
+echo "The card images are created at: $DIR/dist/client/card-images/cards/"
