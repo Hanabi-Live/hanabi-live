b140ce831a731bd865c5dc99e1ec63ebdf6c994c
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-03-01 20:40:43 -0500

    changing filenames of outputted card SVGs

--- client/card-images/createAllCards.ts
+++ client/card-images/createAllCards.ts
@@ -6,6 +6,7 @@
 import fs from "fs";
 import path from "path";
 import { VARIANTS } from "../src/game/data/gameData";
+import { START_CARD_RANK, UNKNOWN_CARD_RANK } from "../src/game/types/constants";
 import drawCards from "../src/game/ui/drawCards";
 import * as drawCardsNode from "./drawCardsNode";
 
@@ -38,7 +39,46 @@ for (const [key, value] of cardImages.entries()) {
 console.log("Finished created all card images.");
 
 for (const [key, value] of allCardImages.entries()) {
-  const filePath = path.join(__dirname, "cards", `${key}.svg`);
+  if (key === "deck-back") {
+    continue;
+  }
+
+  // e.g. "card-Blue-0.svg" --> "b.svg"
+  // e.g. "card-Blue-1.svg" --> "b1.svg"
+  const match = /^(\w+)-(\w+)-(\d)$/.exec(key);
+  let fileName: string |undefined;
+  if (match) {
+    const type = match[1];
+    const suit = match[2];
+    const rankString = match[3];
+    const rank = parseInt(rankString, 10);
+    if (rank === START_CARD_RANK) {
+      continue;
+    }
+
+    let suitAbbrev = suit.toLowerCase()[0];
+    if (suit === "Rainbow") {
+      suitAbbrev = "m";
+    }
+
+    if (type === "card") {
+      if (suit === "Unknown") {
+        // We only care about the real unknown ranks
+        if (rank >= 1 && rank <= 5) {
+          fileName = rank.toString();
+        }
+      } else {
+        fileName = rank === UNKNOWN_CARD_RANK ? suitAbbrev : `${suitAbbrev}${rank}`;
+      }
+    }
+  } else {
+    throw new Error(`Failed to parse the following key: ${key}`);
+  }
+  if (fileName === undefined) {
+    continue;
+  }
+
+  const filePath = path.join(__dirname, "cards", `${fileName}.svg`);
   try {
     fs.writeFileSync(filePath, (value as unknown) as string, "utf8");
   } catch (err) {
