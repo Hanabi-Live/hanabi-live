63e18255ab64b9a023df895ecc24e5a4492e61be
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-03-01 19:58:03 -0500

    better build.sh

--- client/card-images/build.sh
+++ client/card-images/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e # Exit on any errors
+
 # Get the directory of this script
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
