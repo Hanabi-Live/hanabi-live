0663616a8d0746253a82425ef1c43de174011949
NumerateClutter <81528554+NumerateClutter@users.noreply.github.com>
2021-03-29 09:53:14 -0400

    Update server/src/discord.go
    Co-authored-by: Timo <timorcb@gmail.com>

--- server/src/discord.go
+++ server/src/discord.go
@@ -207,7 +207,7 @@ func discordGetChannel(discordID string) string {
 // We need to check for special commands that occur in Discord channels other than #general
 // (because the messages will not flow to the normal "chatCommandMap")
 func discordCheckCommand(ctx context.Context, m *discordgo.MessageCreate) {
-	//There could be a command on any line
+	// There could be a command on any line
 	for _, line := range strings.Split(m.Content, "\n") {
 		// This code is duplicated from the "chatCommand()" function
 		args := strings.Split(line, " ")
