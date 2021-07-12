3264b89ec019a9c157026f738b54e536566408a1
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-07-12 09:35:07 -0400

    updating links

--- server/src/chat_general.go
+++ server/src/chat_general.go
@@ -29,7 +29,7 @@ func chatRules(ctx context.Context, s *Session, d *CommandData, t *Table) {
 
 // /new
 func chatNew(ctx context.Context, s *Session, d *CommandData, t *Table) {
-	msg := "If you are looking to \"get into\" the game and spend a lot of time to play with experienced players, the Hyphen-ated group is always looking for more members. To start with, please read the beginners guide, which goes over how we play and how to join our next game: https://hanabi.github.io/docs/beginner/"
+	msg := "If you are looking to \"get into\" the game and spend a lot of time to play with experienced players, the Hyphen-ated group is always looking for more members. To start with, please read the beginners guide, which goes over how we play and how to join our next game: https://hanabi.github.io/docs/beginner/intro/"
 	// (we can't put "<" or ">" around the link because then it won't display properly in the lobby)
 	chatServerSend(ctx, msg, d.Room, d.NoTablesLock)
 }
