0e4ffb1dfbcffe302f71463efd741f930528478b
Takis Pournaras <pournaras@zilsen.com>
2021-11-30 22:19:47 +0200

    esc when modal shown, jQuery fixes

--- client/src/lobby/keyboardInit.ts
+++ client/src/lobby/keyboardInit.ts
@@ -2,6 +2,7 @@
 
 import * as KeyCode from "keycode-js";
 import globals from "../globals";
+import { isModalVisible } from "../modals";
 import Screen from "./types/Screen";
 
 export default function keyboardInit(): void {
@@ -13,12 +14,18 @@ export default function keyboardInit(): void {
       !$(".ss-search").is(":visible") // Make an exception if the variant dropdown is open
     ) {
       event.preventDefault();
-      $("#create-game-submit").click();
+      $("#create-game-submit").trigger("click");
+      return;
     }
 
     // The rest of the lobby hotkeys only use alt;
     // do not do anything if other modifiers are pressed
     if (event.ctrlKey || event.shiftKey || event.metaKey) {
+      return;
+    }
+
+    // Ignore hotkeys if a modal is open
+    if (isModalVisible()) {
       return;
     }
 
@@ -28,48 +35,48 @@ export default function keyboardInit(): void {
       // Alt + j
       // Click on the first "Join" button in the table list
       if (globals.currentScreen === Screen.Lobby) {
-        $(".lobby-games-join-first-table-button").click();
+        $(".lobby-games-join-first-table-button").trigger("click");
       }
     } else if (event.altKey && event.which === KeyCode.KEY_N) {
       // Alt + n
       // Click the "Create Game" button
       if (globals.currentScreen === Screen.Lobby) {
-        $("#nav-buttons-lobby-create-game").click();
+        $("#nav-buttons-lobby-create-game").trigger("click");
       }
     } else if (event.altKey && event.which === KeyCode.KEY_H) {
       // Alt + h
       // Click the "Show History" button
       if (globals.currentScreen === Screen.Lobby) {
-        $("#nav-buttons-lobby-history").click();
+        $("#nav-buttons-lobby-history").trigger("click");
       }
     } else if (event.altKey && event.which === KeyCode.KEY_A) {
       // Alt + a
       // Click on the "Watch Specific Replay" button
       // (we can't use "Alt + w" because that conflicts with LastPass)
       if (globals.currentScreen === Screen.Lobby) {
-        $("#nav-buttons-lobby-replay").click();
+        $("#nav-buttons-lobby-replay").trigger("click");
       }
     } else if (event.altKey && event.which === KeyCode.KEY_O) {
       // Alt + o
       // Click the "Sign Out" button
       if (globals.currentScreen === Screen.Lobby) {
-        $("#nav-buttons-lobby-sign-out").click();
+        $("#nav-buttons-lobby-sign-out").trigger("click");
       }
     } else if (event.altKey && event.which === KeyCode.KEY_S) {
       // Alt + s
       // Click on the "Start Game" button
       if (globals.currentScreen === Screen.PreGame) {
-        $("#nav-buttons-pregame-start").click();
+        $("#nav-buttons-pregame-start").trigger("click");
       }
     } else if (event.altKey && event.which === KeyCode.KEY_C) {
       if (globals.currentScreen === Screen.PreGame) {
-        $("#nav-buttons-pregame-change-options").click();
+        $("#nav-buttons-pregame-change-options").trigger("click");
       }
     } else if (event.altKey && event.which === KeyCode.KEY_L) {
       // Alt + l
       // Click on the "Leave Game" button
       if (globals.currentScreen === Screen.PreGame) {
-        $("#nav-buttons-pregame-leave").click();
+        $("#nav-buttons-pregame-leave").trigger("click");
       }
     } else if (event.altKey && event.which === KeyCode.KEY_R) {
       // Alt + r
@@ -86,10 +93,10 @@ function clickReturnToLobby() {
   // Click on the "Return to Lobby" button
   // (either at the "game" screen or the "history" screen or the "scores" screen)
   if (globals.currentScreen === Screen.PreGame) {
-    $("#nav-buttons-pregame-unattend").click();
+    $("#nav-buttons-pregame-unattend").trigger("click");
   } else if (globals.currentScreen === Screen.History) {
-    $("#nav-buttons-history-return").click();
+    $("#nav-buttons-history-return").trigger("click");
   } else if (globals.currentScreen === Screen.HistoryOtherScores) {
-    $("#nav-buttons-history-other-scores-return").click();
+    $("#nav-buttons-history-other-scores-return").trigger("click");
   }
 }
