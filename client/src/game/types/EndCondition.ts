cb0bd8ce6dd800d62ab04e71bc7f173d30441368
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-12-09 11:58:31 -0500

    bugfix for end condition in client not matching server

--- client/src/game/types/EndCondition.ts
+++ client/src/game/types/EndCondition.ts
@@ -4,11 +4,11 @@ enum EndCondition {
   Strikeout,
   Timeout,
   Terminated,
-  VotedToKill,
   SpeedrunFail,
   IdleTimeout,
   CharacterSoftlock,
   AllOrNothingFail,
   AllOrNothingSoftlock,
+  VotedToKill,
 }
 export default EndCondition;
