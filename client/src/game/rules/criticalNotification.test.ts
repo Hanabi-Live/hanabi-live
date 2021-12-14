aef9474c94223ff7acd4c073a421a5d3863758c4
DrAnax <takis.pournaras@gmail.com>
2021-12-14 20:55:26 +0200

    test critical discard in Up or Down (#2333)
    * test critical discard in Up or Down
    
    * linter fix
    
    Co-authored-by: DrAnax <pournaras@zilsen.com>

---
+++ client/src/game/rules/criticalNotification.test.ts
@@ -0,0 +1,69 @@
+// Integration tests, involving loading a full game and checking state at different points
+
+import loadGameJSON from "../../../test/loadGameJSON";
+import upOrDownGame from "../../../test_data/up_or_down_critical.json";
+import { getVariant } from "../data/gameData";
+import { START_CARD_RANK } from "../types/constants";
+import State from "../types/State";
+import { isCritical } from "./variants/reversible";
+
+let testState: State;
+
+const getStateAtTurn = (state: State, turn: number) =>
+  state.replay.states[turn];
+const variant = getVariant("Up or Down & Brown (6 Suits)");
+const redSuit = 0;
+
+describe("UI", () => {
+  describe("Up or Down test game", () => {
+    beforeAll(() => {
+      // Load the game and get the final state
+      testState = loadGameJSON(upOrDownGame);
+    });
+
+    describe("at turn 3", () => {
+      test("red S is not critical", () => {
+        const turnState = getStateAtTurn(testState, 2);
+        expect(
+          isCritical(
+            redSuit,
+            START_CARD_RANK,
+            turnState.deck,
+            turnState.playStackDirections,
+            variant,
+          ),
+        ).toBe(false);
+      });
+    });
+
+    describe("at turn 6", () => {
+      test("red 1 is not critical", () => {
+        const turnState = getStateAtTurn(testState, 5);
+        expect(
+          isCritical(
+            redSuit,
+            1,
+            turnState.deck,
+            turnState.playStackDirections,
+            variant,
+          ),
+        ).toBe(false);
+      });
+    });
+
+    describe("at turn 6", () => {
+      test("red 5 is critical", () => {
+        const turnState = getStateAtTurn(testState, 14);
+        expect(
+          isCritical(
+            redSuit,
+            5,
+            turnState.deck,
+            turnState.playStackDirections,
+            variant,
+          ),
+        ).toBe(true);
+      });
+    });
+  });
+});
