babc484f03e670ef3fdd23301a066713f479d011
DrAnax <takis.pournaras@gmail.com>
2022-01-07 19:19:26 +0200

    Update webpack options for CORS (#2443)

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -128,6 +128,15 @@ module.exports = {
   watchOptions: {
     ignored: /node_modules/,
   },
+
+  devServer: {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+    },
+  },
 };
 
 if (!inTravis && sentryTokenIsSet) {
