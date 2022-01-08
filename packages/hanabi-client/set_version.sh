dd2cc8b4a440e372c7dfc9073bf9fc8a29b40b10
Zamiell <5511220+Zamiell@users.noreply.github.com>
2022-01-08 15:07:21 -0500

    fix for set_version script

--- packages/hanabi-client/set_version.sh
+++ packages/hanabi-client/set_version.sh
@@ -4,9 +4,11 @@
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+REPO_ROOT="$DIR/../.."
+
 # Set the version number in the "version.json" file
 # (which is equal to the number of commits in the git repository)
 # This is "baked" into the JavaScript bundle and self-reported when connecting to the server so that
 # the server can deny clients on old versions of the code
 VERSION=$(git rev-list --count HEAD)
-echo "$VERSION" > "$DIR/../data/version.json"
+echo "$VERSION" > "$REPO_ROOT/data/version.json"
