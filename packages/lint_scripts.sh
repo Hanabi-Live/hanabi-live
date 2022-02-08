fced1ba893cc373a7e0320a9208ca087a2584e92
James <5511220+Zamiell@users.noreply.github.com>
2022-02-08 17:43:02 -0500

    Update lint_scripts.sh

--- packages/lint_scripts.sh
+++ packages/lint_scripts.sh
@@ -2,8 +2,17 @@
 
 set -e # Exit on any errors
 
+# Get the directory of this script
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+DIR_CLIENT="$DIR/client"
+DIR_DATA="$DIR/data"
+
+SECONDS=0
+
 lint() {
-  cd $1
+  cd "$1"
 
   # Step 1 - Use Prettier to check formatting
   npx prettier --check "src/**/*.ts"
@@ -16,22 +25,12 @@ lint() {
   # Step 3 - Use ts-prune to check for unused imports
   # The "--error" flag makes it return an error code of 1 if unused exports are found
   # ts-prune should not run, there are many exports in @hanabi/data used in other projects only
-  if [ $# -eq 1 ]
-  then
+  if [ $# -eq 1 ]; then
     npx ts-prune --error
   fi
 }
 
-# Get the directory of this script
-# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
-MAIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-DIR_CLIENT="$MAIN_DIR/client"
-DIR_DATA="$MAIN_DIR/data"
-
-SECONDS=0
-
-lint $DIR_CLIENT
-lint $DIR_DATA "no ts-prune"
+lint "$DIR_CLIENT"
+lint "$DIR_DATA" "no-ts-prune"
 
 echo "Successfully linted in $SECONDS seconds."
