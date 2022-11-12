f44f84b7c881ab7c0aa046f72a54a3904db2c0a1
Evan Chen <evan@evanchen.cc>
2022-11-12 09:10:01 -0500

    [kt] indicators override explicitly

--- packages/client/src/game/ui/HanabiCard.ts
+++ packages/client/src/game/ui/HanabiCard.ts
@@ -821,7 +821,7 @@ export default class HanabiCard
 
   private setTrashMiniIndicator(isTrash: boolean) {
     const known = this.visibleSuitIndex !== null || this.visibleRank !== null;
-    if (isTrash) {
+    if (isTrash && !this.trashcan.isVisible()) {
       this.trashMiniIndicatorTop.visible(!known);
       this.trashMiniIndicatorBottom.visible(known);
     } else {
