ef0471ee1a38d30dd86270182f396e1bf3da5e97
Zamiel <5511220+Zamiell@users.noreply.github.com>
2022-11-19 14:35:25 +0000

    fix: database backup

--- database_backup.sh
+++ database_backup.sh
@@ -46,18 +46,16 @@ gzip "$BACKUPS_DIR/$FILENAME"
 # on Windows).
 function delete_file_if_near_full_local {
   AMOUNT_FULL=$(df "$DIR" | tail -1 | awk '{print $5}' | rev | cut -c 2- | rev)
-  echo "Hard drive amount full: $AMOUNT_FULL"
+  echo "Local hard drive amount full: $AMOUNT_FULL"
   if [[ $AMOUNT_FULL -gt 80 ]]; then
     # Delete the oldest file in the backups directory.
     echo "Hard drive over 80% full; deleting the oldest backup."
     rm "$(ls -t "$BACKUPS_DIR" | tail -1)"
-    return 1
+    delete_file_if_near_full_local
   fi
-
-  return 0
 }
 if uname -a | grep -v MINGW64 >/dev/null 2>&1; then
-  until delete_file_if_near_full_local do done
+  delete_file_if_near_full_local
 fi
 
 # Detect to see if we have Google Drive backups configured (see "INSTALL.md").
@@ -85,15 +83,17 @@ fi
 # Check for free space; if we have less than a gig left, then start deleting
 # old files until we have at least a gig of storage left.
 function delete_file_if_near_full_gdrive {
-  if [[ $($GDRIVE_PATH about --service-account "$GOOGLE_DRIVE_SERVICE_ACCOUNT_FILENAME" | grep Free | grep GB) ]]; then
+  AMOUNT_FULL=$($GDRIVE_PATH about --service-account "$GOOGLE_DRIVE_SERVICE_ACCOUNT_FILENAME" | grep Free)
+  echo "GDrive amount full: $AMOUNT_FULL"
+  if [[ $($AMOUNT_FULL | grep GB) ]]; then
     return
   fi
   echo "Google Drive account has under 1 gig of free space left; deleting the oldest backup."
   OLDEST_FILE_ID=$($GDRIVE_PATH list --service-account "$GOOGLE_DRIVE_SERVICE_ACCOUNT_FILENAME" --no-header --max 9999 --order "createdTime" --query "trashed = false and 'me' in owners and name contains '$REPO'" | head -n 1 | cut -f 1 -d " ")
   $GDRIVE_PATH delete --service-account "$GOOGLE_DRIVE_SERVICE_ACCOUNT_FILENAME" "$OLDEST_FILE_ID"
-  delete_file_if_near_full
+  delete_file_if_near_full_gdrive
 }
-until delete_file_if_near_full_gdrive do done
+delete_file_if_near_full_gdrive
 
 # Upload the file to Google Drive.
 $GDRIVE_PATH upload "$BACKUPS_DIR/$FILENAME.gz" --service-account "$GOOGLE_DRIVE_SERVICE_ACCOUNT_FILENAME" --parent "$GOOGLE_DRIVE_PARENT_DIRECTORY_ID"
