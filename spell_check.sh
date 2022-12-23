b0c407881c47819f76f1b5847d4166112f1e40ed
James <5511220+Zamiell@users.noreply.github.com>
2022-12-22 19:52:23 -0500

    Update spell_check.sh

--- spell_check.sh
+++ spell_check.sh
@@ -9,11 +9,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 
 # Spell check every file using CSpell.
-# We use no-progress and no-summary because we want to only output errors
-#
+# We use no-progress and no-summary because we want to only output errors.
 # We use --gitignore because we want to ignore files which will not be checked into the repo.
 # (The VS Code extension already ignores them by default, so configuring it at the command line
-# will not cause divergence)
+# will not cause divergence.)
 npx cspell lint --no-progress --no-summary --gitignore
 
 # Check for orphaned words.
