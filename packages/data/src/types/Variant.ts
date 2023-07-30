65d9bf76142608e08a1e96c25a6709e2cb280cba
Maximilian Keßler <git@maximilian-kessler.de>
2023-07-30 10:27:48 +0200

    Client: Introduce variant attribute for size of stacks
    This used to be 5 everywhere, but in Sudoku variants, this can now be 4.
    Also, initialing the variant is adjusted now.

---
+++ packages/data/src/types/Variant.ts
@@ -0,0 +1,33 @@
+import { Color } from "./Color";
+import { Suit } from "./Suit";
+
+export interface Variant {
+  readonly name: string;
+  readonly id: number;
+  readonly suits: readonly Suit[];
+  readonly ranks: readonly number[];
+  readonly singleStackSize: number,
+  readonly clueColors: readonly Color[];
+  readonly clueRanks: readonly number[];
+
+  readonly specialRank: number;
+  readonly specialAllClueColors: boolean;
+  readonly specialAllClueRanks: boolean;
+  readonly specialNoClueColors: boolean;
+  readonly specialNoClueRanks: boolean;
+  readonly specialDeceptive: boolean;
+
+  readonly colorCluesTouchNothing: boolean;
+  readonly rankCluesTouchNothing: boolean;
+
+  readonly oddsAndEvens: boolean;
+  readonly funnels: boolean;
+  readonly chimneys: boolean;
+
+  readonly showSuitNames: boolean;
+  readonly maxScore: number;
+  readonly offsetCornerElements: boolean;
+  readonly suitAbbreviations: readonly string[];
+
+  readonly identityNotePattern: string;
+}
