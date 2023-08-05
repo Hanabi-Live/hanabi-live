880aadda98be133a1b01436512ede5faad50ad60
James <5511220+Zamiell@users.noreply.github.com>
2023-08-04 21:13:34 -0400

    fix: lint

--- packages/data/src/utils.ts
+++ packages/data/src/utils.ts
@@ -1,15 +1,3 @@
-export function capitalizeFirstLetter(string: string): string {
-  if (string === "") {
-    return string;
-  }
-
-  const firstCharacter = string.charAt(0);
-  const capitalizedFirstLetter = firstCharacter.toUpperCase();
-  const restOfString = string.slice(1);
-
-  return `${capitalizedFirstLetter}${restOfString}`;
-}
-
 /**
  * Helper function to normalize an integer.
  *
