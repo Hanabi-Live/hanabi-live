85fbf8e821ae3dbe67ef3f69171db4217adb49ff
James <5511220+Zamiell@users.noreply.github.com>
2023-08-05 21:12:59 -0400

    chore: remove superfluous lint config

--- packages/client/.eslintrc.cjs
+++ packages/client/.eslintrc.cjs
@@ -5,10 +5,6 @@ const REPO_ROOT = path.join(__dirname, "..", "..");
 module.exports = {
   extends: [path.join(REPO_ROOT, ".eslintrc.cjs")],
 
-  env: {
-    jquery: true,
-  },
-
   parserOptions: {
     project: path.join(__dirname, "tsconfig.eslint.json"),
   },
