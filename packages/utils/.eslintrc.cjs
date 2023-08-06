5790e7b59f9b86df79065fd45ef4402a11f61971
James <5511220+Zamiell@users.noreply.github.com>
2023-08-05 21:45:05 -0400

    fix: utils lint exceptions

--- packages/utils/.eslintrc.cjs
+++ packages/utils/.eslintrc.cjs
@@ -8,42 +8,4 @@ module.exports = {
   parserOptions: {
     project: path.join(__dirname, "tsconfig.eslint.json"),
   },
-
-  rules: {
-    /**
-     * Documentation:
-     * https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-non-null-assertion.md
-     *
-     * Defined at:
-     * https://isaacscript.github.io/eslint-config-isaacscript
-     *
-     * We use many variables that are only null during initialization; adding explicit type guards
-     * would be superfluous.
-     */
-    "@typescript-eslint/no-non-null-assertion": ["off"],
-
-    /**
-     * Documentation:
-     * https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-cycle.md
-     *
-     * Defined at:
-     * https://isaacscript.github.io/eslint-config-isaacscript
-     *
-     * The codebase uses cyclical dependencies because various objects are attached to the global
-     * variables object, but methods of these objects also reference/change global variables.
-     */
-    "import/no-cycle": ["off"],
-
-    /**
-     * Documentation:
-     * https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/file-extension-in-import.md
-     *
-     * Defined at:
-     * https://isaacscript.github.io/eslint-config-isaacscript
-     *
-     * Keep this rule disabled until the project can be tested to see if it can move to ESM (which
-     * is contingent upon the dependencies being up to date).
-     */
-    "n/file-extension-in-import": "off",
-  },
 };
