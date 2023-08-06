5c3237914eda9ed7fd4ca9de1dc6563525af72cb
James <5511220+Zamiell@users.noreply.github.com>
2023-08-05 21:50:10 -0400

    refactor: simply linting flow

--- lint.sh
+++ lint.sh
@@ -6,6 +6,15 @@ set -euo pipefail # Exit on errors and undefined variables.
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-bash "$DIR/packages/lint_client.sh"
+SECONDS=0
+
+# Client
+bash "$DIR/packages/client/lint.sh"
+bash "$DIR/packages/data/lint.sh"
+bash "$DIR/packages/utils/lint.sh"
+
+# Server
 # Linting of the server is disabled until it is rewritten in TypeScript.
 #bash "$DIR/server/lint_server.sh"
+
+echo "Successfully linted the client packages in $SECONDS seconds."
--- packages/utils/.eslintrc.cjs
+++ packages/utils/.eslintrc.cjs
@@ -8,4 +8,18 @@ module.exports = {
   parserOptions: {
     project: path.join(__dirname, "tsconfig.eslint.json"),
   },
+
+  rules: {
+    /**
+     * Documentation:
+     * https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/file-extension-in-import.md
+     *
+     * Defined at:
+     * https://isaacscript.github.io/eslint-config-isaacscript
+     *
+     * Keep this rule disabled until the project can be tested to see if it can move to ESM (which
+     * is contingent upon the dependencies being up to date).
+     */
+    "n/file-extension-in-import": "off",
+  },
 };
