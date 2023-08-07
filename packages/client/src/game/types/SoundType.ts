07167d44e13cfde954efe140a9de65d0aba65ce8
James <5511220+Zamiell@users.noreply.github.com>
2023-08-07 14:39:08 -0400

    chore: update SoundType comments

--- packages/client/src/game/types/SoundType.ts
+++ packages/client/src/game/types/SoundType.ts
@@ -5,7 +5,10 @@ export enum SoundType {
   // A "normal" or "boring" game action occurred, so do not play any special sound effect.
   Standard,
 
-  // Play sounds
+  // -----------
+  // Play Sounds
+  // -----------
+
   Fail1, // When a misplay (bomb) happens
   Fail2,
   Blind1, // When a blind-play happens
@@ -16,13 +19,20 @@ export enum SoundType {
   Blind6,
   OneOutOfOrder, // When someone performs an "Order Chop Move"
 
+  // --------------
+  // Discard Sounds
+  // --------------
+
   // Discard sounds
   DiscardClued, // When a player discards a card with one or more positive clues on it
   DoubleDiscard, // When a player discards *in* a "double discard" situation
   DoubleDiscardCause, // When a player discards to cause a "double discard" situation
 
-  // Play or discard sounds.
-  Sad, // When the maximum score decreases
+  // ---------------------
+  // Play / Discard Sounds
+  // ---------------------
+
+  Sad, // When the maximum score decreases.
   // (This normally happens when discarding a critical card, but it can also happen from playing a
   // card in some variants.)
 
@@ -31,8 +41,11 @@ export enum SoundType {
   Oink, // For "Cow & Pig" variants
   Quack, // For "Duck" variants
 
-  // Finished
-  FinishedSuccess, // When the game is finished with any score other than 0 or the maximum score
-  FinishedFail, // When the game is finished with a score of 0
-  FinishedPerfect, // When the game is finished with the maximum score possible
+  // ---------------
+  // Finished Sounds
+  // ---------------
+
+  FinishedSuccess, // When the game is finished with any score other than 0 or the maximum score.
+  FinishedFail, // When the game is finished with a score of 0.
+  FinishedPerfect, // When the game is finished with the maximum score possible.
 }
