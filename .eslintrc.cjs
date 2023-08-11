b027154325be56d9f84202234ef068171edf8584
James <5511220+Zamiell@users.noreply.github.com>
2023-08-11 14:52:49 -0400

    chore: document @typescript-eslint/prefer-enum-initializers

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -56,6 +56,18 @@ module.exports = {
      * We use number enums to save bandwidth between client and server. Number enums are almost
      * always safe with the `isaacscript/strict-enums` rule.
      */
+    "@typescript-eslint/prefer-enum-initializers": "off",
+
+    /**
+     * Documentation:
+     * https://github.com/IsaacScript/isaacscript/blob/main/packages/eslint-plugin-isaacscript/docs/rules/no-number-enums.md
+     *
+     * Defined at:
+     * https://isaacscript.github.io/eslint-config-isaacscript
+     *
+     * We use number enums to save bandwidth between client and server. Number enums are almost
+     * always safe with the `isaacscript/strict-enums` rule.
+     */
     "isaacscript/no-number-enums": "off",
 
     /**
