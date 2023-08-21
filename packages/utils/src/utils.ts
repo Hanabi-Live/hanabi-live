bab963b5d912b931ad37fa7a32913d135c2b3f71
James <5511220+Zamiell@users.noreply.github.com>
2023-08-21 14:27:10 -0400

    chore: update filterMap description

--- packages/utils/src/utils.ts
+++ packages/utils/src/utils.ts
@@ -45,8 +45,8 @@ export function eRange(start: number, end?: number, increment = 1): number[] {
  * function that transforms a value, but return `undefined` if the value should be skipped. (Thus,
  * this function cannot be used in situations where `undefined` can be a valid array element.)
  *
- * This function is useful because a map will always produce an array with the same amount of
- * elements as the original array.
+ * This function is useful because the `Array.map` method will always produce an array with the same
+ * amount of elements as the original array.
  *
  * This is named `filterMap` after the Rust function:
  * https://doc.rust-lang.org/std/iter/struct.FilterMap.html
