1a80924ebb7edd6cc34919088fd51377d4ab7037
James <5511220+Zamiell@users.noreply.github.com>
2023-08-23 14:12:40 -0400

    fix: lint

--- packages/client/src/utils.ts
+++ packages/client/src/utils.ts
@@ -37,10 +37,6 @@ export function getHTMLInputElement(selectors: string): HTMLInputElement {
   return element;
 }
 
-export function getRandomNumber(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
 export function getURLFromPath(path: string): string {
   let url = `${window.location.protocol}//${window.location.hostname}`;
   if (window.location.port !== "") {
