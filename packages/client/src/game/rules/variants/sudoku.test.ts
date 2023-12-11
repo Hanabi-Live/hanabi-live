d941607be477efc041e14d1b251f913c3f2d8bef
Maximilian Keßler <git@maximilian-kessler.de>
2023-12-12 00:26:33 +0100

    Add tests for 4-suit sudoku

--- packages/client/src/game/rules/variants/sudoku.test.ts
+++ packages/client/src/game/rules/variants/sudoku.test.ts
@@ -68,3 +68,62 @@ describe("sudokuWalkUpAll5", () => {
     expect(maxScoresForEachStartingValueOfSuit).toStrictEqual([0, 0, 2, 1, 0]);
   });
 });
+
+describe("sudokuWalkUpAll4", () => {
+  const variant = getVariant("Sudoku (4 Suits)");
+  test("Handles single discarded 1", () => {
+    const allDiscardedSet: Set<Rank> = new Set<Rank>([1]);
+    const { allMax, maxScoresForEachStartingValueOfSuit } =
+        sudokuWalkUpAll(allDiscardedSet, variant);
+    expect(allMax).toBe(false);
+    expect(maxScoresForEachStartingValueOfSuit).toStrictEqual([0, 3, 2, 1]);
+  });
+
+  test("Handles single discarded 2", () => {
+    const allDiscardedSet: Set<Rank> = new Set<Rank>([2]);
+    const { allMax, maxScoresForEachStartingValueOfSuit } =
+        sudokuWalkUpAll(allDiscardedSet, variant);
+    expect(allMax).toBe(false);
+    expect(maxScoresForEachStartingValueOfSuit).toStrictEqual([1, 0, 3, 2]);
+  });
+
+  test("Handles single discarded 3", () => {
+    const allDiscardedSet: Set<Rank> = new Set<Rank>([3]);
+    const { allMax, maxScoresForEachStartingValueOfSuit } =
+        sudokuWalkUpAll(allDiscardedSet, variant);
+    expect(allMax).toBe(false);
+    expect(maxScoresForEachStartingValueOfSuit).toStrictEqual([2, 1, 0, 3]);
+  });
+
+  test("Handles single discarded 4", () => {
+    const allDiscardedSet: Set<Rank> = new Set<Rank>([4]);
+    const { allMax, maxScoresForEachStartingValueOfSuit } =
+        sudokuWalkUpAll(allDiscardedSet, variant);
+    expect(allMax).toBe(false);
+    expect(maxScoresForEachStartingValueOfSuit).toStrictEqual([3, 2, 1, 0]);
+  });
+
+  test("Handles discarded 2 and 4", () => {
+    const allDiscardedSet: Set<Rank> = new Set<Rank>([2, 4]);
+    const { allMax, maxScoresForEachStartingValueOfSuit } =
+        sudokuWalkUpAll(allDiscardedSet, variant);
+    expect(allMax).toBe(false);
+    expect(maxScoresForEachStartingValueOfSuit).toStrictEqual([1, 0, 1, 0]);
+  });
+
+  test("Handles discarded 1 and 4", () => {
+    const allDiscardedSet: Set<Rank> = new Set<Rank>([1, 4]);
+    const { allMax, maxScoresForEachStartingValueOfSuit } =
+        sudokuWalkUpAll(allDiscardedSet, variant);
+    expect(allMax).toBe(false);
+    expect(maxScoresForEachStartingValueOfSuit).toStrictEqual([0, 2, 1, 0]);
+  });
+
+  test("Handles discarded 1, 2, and 4", () => {
+    const allDiscardedSet: Set<Rank> = new Set<Rank>([1, 2, 4]);
+    const { allMax, maxScoresForEachStartingValueOfSuit } =
+        sudokuWalkUpAll(allDiscardedSet, variant);
+    expect(allMax).toBe(false);
+    expect(maxScoresForEachStartingValueOfSuit).toStrictEqual([0, 0, 1, 0]);
+  });
+});
