7a0c3c2c7858ce1f6a79fb087db85abec0c5b45d
James <5511220+Zamiell@users.noreply.github.com>
2023-12-13 23:39:21 -0500

    refactor: dynamic SuitIndex

--- packages/data/src/types/PlayerIndex.ts
+++ packages/data/src/types/PlayerIndex.ts
@@ -1,4 +1,5 @@
 import type { ERange } from "@hanabi/utils";
 import type { MAX_PLAYERS } from "../constants";
 
+/** The maximum number of players in a game is 6. Thus, the valid player indexes are 0 through 5. */
 export type PlayerIndex = ERange<0, typeof MAX_PLAYERS>;
--- packages/data/src/types/SuitIndex.ts
+++ packages/data/src/types/SuitIndex.ts
@@ -1,2 +1,5 @@
+import type { ERange } from "@hanabi/utils";
+import type { MAX_SUITS_IN_A_VARIANT } from "../constants";
+
 /** The maximum number of suits in a variant is 6. Thus, the valid suit indexes are 0 through 5. */
-export type SuitIndex = 0 | 1 | 2 | 3 | 4 | 5;
+export type SuitIndex = ERange<0, typeof MAX_SUITS_IN_A_VARIANT>;
