e7e4bc592be45627db9eb654f6638514e2224e1c
Maximilian Keßler <git@maximilian-kessler.de>
2023-12-16 14:51:01 +0100

    Add assertions regarding retrieval of cards from globals.deck
    Since we believe that
    https://github.com/Hanabi-Live/hanabi-live/issues/2894
    is related to getCardOrStackBase returning 'wrong' cards,
    i.e. cards that are not marked 'isStackBase' where we would expect so,
    we introduce additional checks upon calling this method to hopefully
    catch errors sooner.
    
    Additionally, we check that `globals.deck` never gets too long.

--- packages/client/src/game/ui/getCardOrStackBase.ts
+++ packages/client/src/game/ui/getCardOrStackBase.ts
@@ -1,20 +1,32 @@
 import type { CardOrder } from "@hanabi/data";
+import { assertDefined } from "@hanabi/utils";
 import * as deckRules from "../rules/deck";
 import type { HanabiCard } from "./HanabiCard";
 import { globals } from "./UIGlobals";
 
-export function getCardOrStackBase(order: CardOrder): HanabiCard | undefined {
-  const card = globals.deck[order];
-  if (card !== undefined) {
+export function getCardOrStackBase(order: CardOrder): HanabiCard {
+  const numTotalCards = deckRules.totalCards(globals.variant);
+  if (order < numTotalCards) {
+    const card = globals.deck[order];
+    assertDefined(card, `Failed to get card of order ${order} from the deck.`);
+    if (card.isStackBase) {
+      throw Error("Unexpected card marked stackBase found in globals.deck.");
+    }
     return card;
   }
 
   // Stack bases use the orders after the final card in the deck.
-  const stackBaseIndex = order - deckRules.totalCards(globals.variant);
+  const stackBaseIndex = order - numTotalCards;
   const stackBase = globals.stackBases[stackBaseIndex];
-  if (stackBase !== undefined) {
-    return stackBase;
+  assertDefined(
+    stackBase,
+    `Failed to get stack base of index ${stackBaseIndex}`,
+  );
+  if (!stackBase.isStackBase) {
+    throw Error(
+      "Unexpected card not marked stackBase found in globals.stackBases.",
+    );
   }
 
-  return undefined;
+  return stackBase;
 }
