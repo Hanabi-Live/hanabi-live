Hanabi-Live/hanabi-live

commit ad384ac1004842b73d8a6cd4c65868a656fb600c

refactor: ClientAction is now union

packages/client/src/lobby/createReplayJSON.ts
@@ -1,11 +1,22 @@
-import type { PlayerIndex } from "@hanabi/data";
+import type {
+  CardOrder,
+  ColorIndex,
+  PlayerIndex,
+  RankClueNumber,
+  Variant,
+} from "@hanabi/data";
 import { DEFAULT_PLAYER_NAMES, SITE_URL } from "@hanabi/data";
 import type { LogEntry } from "@hanabi/game";
 import { ClueType } from "@hanabi/game";
 import { assertDefined, parseIntSafe } from "@hanabi/utils";
+import { includes } from "lodash";
 import { SelfChatMessageType, sendSelfPMFromServer } from "../chat";
+import { cardsPerHand } from "../game/rules/hand";
 import { ActionType } from "../game/types/ActionType";
-import type { ClientAction } from "../game/types/ClientAction";
+import type {
+  ClientAction,
+  ClientActionClue,
+} from "../game/types/ClientAction";
 import type { JSONGame } from "../game/types/JSONGame";
 import type { ReplayState } from "../game/types/ReplayState";
 import { globals } from "../game/ui/UIGlobals";
@@ -208,38 +219,43 @@ function getActionFromLogEntry(
   logEntry: LogEntry,
 ): ClientAction | undefined {
   const foundPlay = logEntry.text.match(PLAY_REGEX);
+  if (foundPlay !== null) {
+    const [, playerName, slotString] = foundPlay;
+    if (playerName !== undefined && slotString !== undefined) {
+      const slot = parseIntSafe(slotString);
+      if (slot !== undefined) {
+        return getActionFromHypoPlayOrDiscard(
+          i,
+          ActionType.Play,
+          playerName,
+          slot,
+        );
+      }
+    }
+  }
+
   const foundDiscard = logEntry.text.match(DISCARD_REGEX);
+  if (foundDiscard !== null) {
+    const [, playerName, slotString] = foundDiscard;
+    if (playerName !== undefined && slotString !== undefined) {
+      const slot = parseIntSafe(slotString);
+      if (slot !== undefined) {
+        return getActionFromHypoPlayOrDiscard(
+          i,
+          ActionType.Discard,
+          playerName,
+          slot,
+        );
+      }
+    }
+  }
+
   const foundClue = logEntry.text.match(CLUE_REGEX);
-
-  if (foundPlay !== null && foundPlay.length > 2) {
-    const target = parseIntSafe(foundPlay[2]!);
-    assertDefined(target, `Failed to parse the play target: ${foundPlay[2]}`);
-
-    return getActionFromHypoPlayOrDiscard(
-      i,
-      ActionType.Play,
-      foundPlay[1]!,
-      target,
-    );
-  }
-
-  if (foundDiscard !== null && foundDiscard.length > 2) {
-    const target = parseIntSafe(foundDiscard[2]!);
-    assertDefined(
-      target,
-      `Failed to parse the discard target: ${foundDiscard[2]}`,
-    );
-
-    return getActionFromHypoPlayOrDiscard(
-      i,
-      ActionType.Discard,
-      foundDiscard[1]!,
-      target,
-    );
-  }
-
-  if (foundClue !== null && foundClue.length > 2) {
-    return getActionFromHypoClue(foundClue[1]!, foundClue[2]!);
+  if (foundClue !== null) {
+    const [, playerName, clueWord] = foundClue;
+    if (playerName !== undefined && clueWord !== undefined) {
+      return getActionFromHypoClue(playerName, clueWord);
+    }
   }
 
   return undefined;
@@ -247,11 +263,11 @@ function getActionFromLogEntry(
 
 function getActionFromHypoPlayOrDiscard(
   entryNumber: number,
-  actionType: ActionType,
+  actionType: ActionType.Play | ActionType.Discard,
   playerName: string,
   slot: number,
 ): ClientAction {
-  const playerIndex = getPlayerIndexFromName(playerName);
+  const playerIndex = getPlayerIndexFromPlayerName(playerName);
   assertDefined(
     playerIndex,
     `Failed to find the player index corresponding to: ${playerName}`,
@@ -259,78 +275,110 @@ function getActionFromHypoPlayOrDiscard(
 
   // Go to previous hypo state to find the card. Cards are stored in reverse order than the one
   // perceived.
-  const cardsPerPlayer = getCardsPerPlayer();
-  const slotIndex = cardsPerPlayer - slot;
-  const cardID = getCardFromHypoState(entryNumber - 1, playerIndex, slotIndex);
+  const numCards = cardsPerHand(globals.options);
+  const slotIndex = numCards - slot;
+  const cardOrder = getCardOrderFromHypoState(
+    entryNumber - 1,
+    playerIndex,
+    slotIndex,
+  );
+  assertDefined(
+    cardOrder,
+    "Failed to get the card order from the last hypothetical state.",
+  );
+
   return {
     type: actionType,
-    target: cardID,
+    target: cardOrder,
   };
 }
 
-function getActionFromHypoClue(playerName: string, clue: string): ClientAction {
-  const playerIndex = getPlayerIndexFromName(playerName);
+function getActionFromHypoClue(
+  playerName: string,
+  clueWord: string,
+): ClientActionClue {
+  const playerIndex = getPlayerIndexFromPlayerName(playerName);
   assertDefined(
     playerIndex,
     `Failed to find the player index corresponding to: ${playerName}`,
   );
 
-  let parsedClue = parseIntSafe(clue);
+  let clueNumber = parseIntSafe(clueWord);
 
   // "Odds and Evens" give "Odd" or "Even" as rank clues.
-  if (clue === "Odd") {
-    parsedClue = 1;
-  } else if (clue === "Even") {
-    parsedClue = 2;
+  if (clueWord === "Odd") {
+    clueNumber = 1;
+  } else if (clueWord === "Even") {
+    clueNumber = 2;
   }
 
-  if (parsedClue === undefined) {
+  if (clueNumber === undefined) {
     // It is a color clue.
+    const colorIndex = getColorIndexFromColorName(clueWord, globals.variant);
+    assertDefined(
+      colorIndex,
+      `The clue color name of ${clueWord} is not valid for this variant.`,
+    );
+
     return {
       type: ActionType.ColorClue,
       target: playerIndex,
-      value: getColorIdFromString(clue),
+      value: colorIndex,
     };
   }
+
+  if (!includes(globals.variant.clueRanks, clueNumber)) {
+    throw new Error(
+      `The clue rank of ${clueNumber} is not valid for this variant.`,
+    );
+  }
+  const rankClueNumber = clueNumber as RankClueNumber;
 
   // It is a rank clue.
   return {
     type: ActionType.RankClue,
     target: playerIndex,
-    value: parsedClue,
+    value: rankClueNumber,
   };
 }
 
-function getPlayerIndexFromName(name: string): PlayerIndex | undefined {
-  const playerIndex = globals.metadata.playerNames.indexOf(name);
+function getPlayerIndexFromPlayerName(
+  playerName: string,
+): PlayerIndex | undefined {
+  const playerIndex = globals.metadata.playerNames.indexOf(playerName);
   return playerIndex === -1 ? undefined : (playerIndex as PlayerIndex);
 }
 
-function getCardFromHypoState(
+function getCardOrderFromHypoState(
   previousStateIndex: number,
   playerIndex: PlayerIndex,
   slotIndex: number,
-): number {
+): CardOrder | undefined {
   if (globals.state.replay.hypothetical === null) {
-    return 0;
+    return undefined;
   }
 
   const stateIndex = Math.max(previousStateIndex, 0);
-  const stateOnHypoTurn = globals.state.replay.hypothetical.states[stateIndex]!;
-  const playerHand = stateOnHypoTurn.hands[playerIndex]!;
-  return playerHand[slotIndex]!;
-}
-
-function getColorIdFromString(clue: string): number {
-  let suitIndex = 0;
-  for (const [index, color] of globals.variant.clueColors.entries()) {
-    if (clue.startsWith(color.name)) {
-      suitIndex = index;
-    }
+  const stateOnHypoTurn = globals.state.replay.hypothetical.states[stateIndex];
+  if (stateOnHypoTurn === undefined) {
+    return undefined;
   }
-  return suitIndex;
+
+  const playerHand = stateOnHypoTurn.hands[playerIndex];
+  if (playerHand === undefined) {
+    return undefined;
+  }
+
+  return playerHand[slotIndex];
 }
 
-function getCardsPerPlayer(): number {
-  return globals.state.replay.states[0]!.hands[0]!.length;
+function getColorIndexFromColorName(
+  colorName: string,
+  variant: Variant,
+): ColorIndex | undefined {
+  const colorIndex = variant.clueColors.findIndex(
+    (clueColor) => clueColor.name === colorName,
+  );
+
+  return colorIndex === -1 ? undefined : (colorIndex as ColorIndex);
 }

packages/client/src/game/types/ActionType.ts
@@ -1,3 +1,4 @@
+/** Sent to the server in the "action" command. */
 export enum ActionType {
   Play,
   Discard,

packages/client/src/game/types/ClientAction.ts
@@ -1,12 +1,44 @@
-import type { CardOrder } from "@hanabi/data";
+import type {
+  CardOrder,
+  PlayerIndex,
+  RankClueNumber,
+  SuitIndex,
+} from "@hanabi/data";
+import type { CompositionTypeSatisfiesEnum } from "@hanabi/utils";
 import type { ActionType } from "./ActionType";
 
 /** A message sent to the server that represents the in-game action that we just took. */
-export interface ClientAction {
-  type: ActionType;
+export type ClientAction =
+  | ClientActionPlay
+  | ClientActionDiscard
+  | ClientActionColorClue
+  | ClientActionRankClue;
 
-  /** The player index of the player clued or the card order of the card played/discarded. */
-  target: number | CardOrder;
+type _Test = CompositionTypeSatisfiesEnum<
+  ClientAction,
+  Exclude<ActionType, ActionType.GameOver>
+>;
 
-  value?: number;
+interface ClientActionPlay {
+  type: ActionType.Play;
+  target: CardOrder;
 }
+
+interface ClientActionDiscard {
+  type: ActionType.Discard;
+  target: CardOrder;
+}
+
+export interface ClientActionColorClue {
+  type: ActionType.ColorClue;
+  target: PlayerIndex;
+  value: SuitIndex;
+}
+
+export interface ClientActionRankClue {
+  type: ActionType.RankClue;
+  target: PlayerIndex;
+  value: RankClueNumber;
+}
+
+export type ClientActionClue = ClientActionColorClue | ClientActionRankClue;

packages/client/src/game/ui/Deck.ts
@@ -1,3 +1,4 @@
+import type { CardOrder } from "@hanabi/data";
 import Konva from "konva";
 import * as tooltips from "../../tooltips";
 import { OptionIcons } from "../../types/OptionIcons";
@@ -91,10 +92,12 @@ export class Deck extends Konva.Group {
         },
       });
     } else if (draggedTo === "playArea") {
+      // Card orders start at 0, so the final card order is the length of the deck - 1.
+      const numCardsInDeck = deckRules.totalCards(globals.variant);
+      const cardOrder = (numCardsInDeck - 1) as CardOrder;
       turn.end({
         type: ActionType.Play,
-        // Card orders start at 0, so the final card order is the length of the deck - 1.
-        target: deckRules.totalCards(globals.variant) - 1,
+        target: cardOrder,
       });
     }
   }

packages/client/src/game/ui/HanabiCardClickSpeedrun.ts
@@ -2,7 +2,7 @@
 
 import type { Color } from "@hanabi/data";
 import { START_CARD_RANK } from "@hanabi/data";
-import { assertDefined } from "@hanabi/utils";
+import { assertDefined, todo } from "@hanabi/utils";
 import * as cardRules from "../rules/card";
 import * as clueTokensRules from "../rules/clueTokens";
 import { ActionType } from "../types/ActionType";
@@ -102,14 +102,19 @@ function clickLeft(card: HanabiCard, event: MouseEvent) {
       }
     }
 
+    // Use the first color as a default.
     const colorIndex =
       clueColor === undefined
-        ? 0 // Use the color at index 0 as a default.
-        : colorToColorIndex(clueColor, globals.variant);
+        ? 0
+        : colorToColorIndex(clueColor, globals.variant) ?? 0;
+
+    if (typeof card.state.location !== "number") {
+      return;
+    }
 
     turn.end({
       type: ActionType.ColorClue,
-      target: card.state.location as number,
+      target: card.state.location,
       value: colorIndex,
     });
   }
@@ -143,6 +148,7 @@ function clickRight(card: HanabiCard, event: MouseEvent) {
 
   // Right-clicking on cards in other people's hands is a rank clue action.
   if (
+    typeof card.state.location === "number" &&
     card.state.location !== globals.metadata.ourPlayerIndex &&
     cardRules.isCardInPlayerHand(card.state) &&
     card.state.rank !== null &&
@@ -158,7 +164,7 @@ function clickRight(card: HanabiCard, event: MouseEvent) {
   ) {
     turn.end({
       type: ActionType.RankClue,
-      target: card.state.location as number,
+      target: card.state.location,
       value: card.state.rank,
     });
     return;
@@ -180,5 +186,8 @@ function clickRight(card: HanabiCard, event: MouseEvent) {
   // moved".)
   if (!event.ctrlKey && !event.shiftKey && event.altKey && !event.metaKey) {
     card.appendNote("cm");
+    return;
   }
+
+  todo();
 }

packages/client/src/game/ui/clues.ts
@@ -5,6 +5,10 @@ import { getCharacterNameForPlayer } from "../reducers/reducerHelpers";
 import * as clueTokensRules from "../rules/clueTokens";
 import * as cluesRules from "../rules/clues";
 import { ActionType } from "../types/ActionType";
+import type {
+  ClientActionColorClue,
+  ClientActionRankClue,
+} from "../types/ClientAction";
 import type { Clue } from "../types/Clue";
 import type { MsgClue } from "../types/MsgClue";
 import type { ColorButton } from "./ColorButton";
@@ -44,7 +48,7 @@ export function checkLegal(): void {
     return;
   }
 
-  const who = target.targetIndex;
+  const who = target.targetPlayerIndex;
   const { currentPlayerIndex } = globals.state.visibleState!.turn;
   if (currentPlayerIndex === null) {
     return;
@@ -154,22 +158,22 @@ export function give(): void {
       ? globals.elements.clueTargetButtonGroup
       : globals.elements.clueTargetButtonGroup2;
 
-  const target = clueTargetButtonGroup?.getPressed() as
+  const playerButton = clueTargetButtonGroup?.getPressed() as
     | PlayerButton
     | null
     | undefined;
   const { clueTypeButtonGroup } = globals.elements;
-  const clueButton = clueTypeButtonGroup?.getPressed() as
+  const clueOrRankButton = clueTypeButtonGroup?.getPressed() as
     | ColorButton
     | RankButton
     | null
     | undefined;
 
   if (
-    target === undefined ||
-    target === null ||
-    clueButton === undefined ||
-    clueButton === null
+    playerButton === undefined ||
+    playerButton === null ||
+    clueOrRankButton === undefined ||
+    clueOrRankButton === null
   ) {
     // They have not selected a player or a clue type.
     globals.elements.giveClueButton!.setEnabled(false);
@@ -182,38 +186,38 @@ export function give(): void {
 
   globals.elements.giveClueButton!.setEnabled(false);
 
-  let type: ActionType;
-  let value: number;
-
-  switch (clueButton.clue.type) {
+  switch (clueOrRankButton.clue.type) {
     case ClueType.Color: {
       const colorIndex = colorToColorIndex(
-        clueButton.clue.value,
+        clueOrRankButton.clue.value,
         globals.variant,
       );
       assertDefined(
         colorIndex,
-        `Failed to get the color index for color: ${clueButton.clue.value.name}`,
+        `Failed to get the color index for color: ${clueOrRankButton.clue.value.name}`,
       );
 
-      type = ActionType.ColorClue;
-      value = colorIndex;
+      const clientActionColorClue: ClientActionColorClue = {
+        type: ActionType.ColorClue,
+        target: playerButton.targetPlayerIndex,
+        value: colorIndex,
+      };
+      turn.end(clientActionColorClue);
+
       break;
     }
 
     case ClueType.Rank: {
-      type = ActionType.RankClue;
-      value = clueButton.clue.value; // eslint-disable-line @typescript-eslint/prefer-destructuring
+      const clientActionRankClue: ClientActionRankClue = {
+        type: ActionType.RankClue,
+        target: playerButton.targetPlayerIndex,
+        value: clueOrRankButton.clue.value,
+      };
+      turn.end(clientActionRankClue);
+
       break;
     }
   }
-
-  // Send the message to the server.
-  turn.end({
-    type,
-    target: target.targetIndex,
-    value,
-  });
 }
 
 function shouldGiveClue() {

packages/client/src/game/ui/controls/PlayerButton.ts
@@ -1,14 +1,15 @@
+import type { PlayerIndex } from "@hanabi/data";
 import Konva from "konva";
 import { Button } from "./Button";
 
 export class PlayerButton extends Button {
-  targetIndex: number;
+  targetPlayerIndex: PlayerIndex;
   radioBackground: Konva.Circle;
   radioDot: Konva.Circle;
 
-  constructor(config: Konva.ContainerConfig, targetIndex: number) {
+  constructor(config: Konva.ContainerConfig, targetPlayerIndex: PlayerIndex) {
     super(config, []);
-    this.targetIndex = targetIndex;
+    this.targetPlayerIndex = targetPlayerIndex;
 
     const x = this.height() / 2;
     this.radioBackground = new Konva.Circle({

packages/client/src/game/ui/convert.ts
@@ -1,7 +1,7 @@
 // These are helper functions that convert objects to the integers that the server expects and vice
 // versa.
 
-import type { Color, Suit, SuitIndex, Variant } from "@hanabi/data";
+import type { Color, ColorIndex, Suit, SuitIndex, Variant } from "@hanabi/data";
 
 export function suitIndexToSuit(
   suitIndex: SuitIndex | null,
@@ -17,7 +17,7 @@ export function suitIndexToSuit(
 export function colorToColorIndex(
   color: Color,
   variant: Variant,
-): number | undefined {
+): ColorIndex | undefined {
   const colorIndex = variant.clueColors.indexOf(color);
-  return colorIndex === -1 ? undefined : colorIndex;
+  return colorIndex === -1 ? undefined : (colorIndex as ColorIndex);
 }

commit 686f1e2fbc255323f359aa2e1eddd26f45e45db9

fix parsing colors in hypos
This fixes the bug introduced in ad384ac during refactoring,
where the `startsWith` method was replaced with an exact match.

packages/client/src/lobby/createReplayJSON.ts
@@ -375,8 +375,8 @@ function getColorIndexFromColorName(
   colorName: string,
   variant: Variant,
 ): ColorIndex | undefined {
-  const colorIndex = variant.clueColors.findIndex(
-    (clueColor) => clueColor.name === colorName,
+  const colorIndex = variant.clueColors.findIndex((clueColor) =>
+    colorName.startsWith(clueColor.name),
   );
 
   return colorIndex === -1 ? undefined : (colorIndex as ColorIndex);