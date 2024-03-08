79a95249366362633c24909af5e25888364b92fa
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-03-08 10:44:55 -0500

    feat: kt hotkey

--- packages/client/src/game/ui/clickNotes.ts
+++ packages/client/src/game/ui/clickNotes.ts
@@ -10,50 +10,35 @@ export function clickRightCheckAddNote(
   card: HanabiCard,
   isSpeedrun: boolean,
 ): void {
+  // Note hotkeys should only work in ongoing games.
+  if (globals.state.finished) {
+    return;
+  }
+
+  // Ctrl + shift + alt + right-click is a "kt" note. (This is a common abbreviation for "this card
+  // is known trash".)
+  if (event.ctrlKey && event.shiftKey && event.altKey && !event.metaKey) {
+    lastNote = "kt";
+    card.appendNote(lastNote);
+    return;
+  }
+
   // Ctrl + shift + right-click is a shortcut for entering the same note as previously entered.
   // (This must be above the other note code because of the modifiers.)
-  if (
-    event.ctrlKey &&
-    event.shiftKey &&
-    !event.metaKey &&
-    !globals.state.finished
-  ) {
-    if (event.altKey) {
-      // When Alt is held, copy only the new part of the last note.
-      if (lastNote === "") {
-        const noteText = globals.lastNote;
-        const lastPipe = noteText.lastIndexOf("|");
-        const lastNoteString = noteText.slice(lastPipe + 1).trim();
-        lastNote = lastNoteString;
-      }
-      card.appendNoteOnly(lastNote);
-    } else {
-      card.setNote(globals.lastNote);
-    }
+  if (event.ctrlKey && event.shiftKey && !event.altKey && !event.metaKey) {
+    card.setNote(globals.lastNote);
     return;
   }
 
   // Ctrl + alt + right-click is prepend turn count.
-  if (
-    event.ctrlKey &&
-    !event.shiftKey &&
-    event.altKey &&
-    !event.metaKey &&
-    !globals.state.finished
-  ) {
+  if (event.ctrlKey && !event.shiftKey && event.altKey && !event.metaKey) {
     lastNote = `#${globals.elements.turnNumberLabel?.text()}`;
     card.prependTurnCountNote(lastNote);
     return;
   }
 
   // Shift + right-click is a "f" note. (This is a common abbreviation for "this card is Finessed".)
-  if (
-    !event.ctrlKey &&
-    event.shiftKey &&
-    !event.altKey &&
-    !event.metaKey &&
-    !globals.state.finished
-  ) {
+  if (!event.ctrlKey && event.shiftKey && !event.altKey && !event.metaKey) {
     lastNote = "f";
     card.appendNote(lastNote);
     return;
@@ -61,13 +46,7 @@ export function clickRightCheckAddNote(
 
   // Alt + right-click is a "cm" note. (This is a common abbreviation for "this card is chop
   // moved".)
-  if (
-    !event.ctrlKey &&
-    !event.shiftKey &&
-    event.altKey &&
-    !event.metaKey &&
-    !globals.state.finished
-  ) {
+  if (!event.ctrlKey && !event.shiftKey && event.altKey && !event.metaKey) {
     lastNote = "cm";
     card.appendNote(lastNote);
     return;
@@ -78,8 +57,7 @@ export function clickRightCheckAddNote(
     event.ctrlKey === isSpeedrun &&
     !event.shiftKey &&
     !event.altKey &&
-    !event.metaKey &&
-    !globals.state.finished
+    !event.metaKey
   ) {
     preOpenNoteEditTooltip(card);
     return;
