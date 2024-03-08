872253e896de578e1526ad337414d0fbe4837576
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-03-08 10:13:02 -0500

    fix: spectator note error

--- packages/client/src/game/ui/getCardOrStackBase.ts
+++ packages/client/src/game/ui/getCardOrStackBase.ts
@@ -1,34 +1,21 @@
 import type { CardOrder } from "@hanabi/game";
 import { getTotalCardsInDeck } from "@hanabi/game";
-import { assertDefined } from "isaacscript-common-ts";
 import type { HanabiCard } from "./HanabiCard";
 import { globals } from "./UIGlobals";
 
 export function getCardOrStackBase(order: CardOrder): HanabiCard | undefined {
   const totalCardsInDeck = getTotalCardsInDeck(globals.variant);
-  if (order < totalCardsInDeck) {
-    const card = globals.deck[order];
-    assertDefined(card, `Failed to get card of order ${order} from the deck.`);
-    if (card.isStackBase) {
-      throw new Error(
-        "Unexpected card marked stackBase found in globals.deck.",
-      );
-    }
-    return card;
-  }
 
   // Stack bases use the orders after the final card in the deck.
-  const stackBaseIndex = order - totalCardsInDeck;
-  const stackBase = globals.stackBases[stackBaseIndex];
-  assertDefined(
-    stackBase,
-    `Failed to get stack base of index ${stackBaseIndex}`,
-  );
-  if (!stackBase.isStackBase) {
-    throw new Error(
-      "Unexpected card not marked stackBase found in globals.stackBases.",
-    );
+  if (order >= totalCardsInDeck) {
+    const stackBaseIndex = order - totalCardsInDeck;
+    const stackBase = globals.stackBases[stackBaseIndex];
+    return stackBase === undefined || !stackBase.isStackBase
+      ? undefined
+      : stackBase;
   }
 
-  return stackBase;
+  // This is a normal card.
+  const card = globals.deck[order];
+  return card === undefined || card.isStackBase ? undefined : card;
 }
