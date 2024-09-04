6d551263dfd9aed2d4b09ecad0a1704ff37d2dcd
progiv <gmvano@gmail.com>
2024-09-04 15:08:02 +0300

    Join pre-game with stored join password (#2963)

--- packages/client/src/lobby/url.ts
+++ packages/client/src/lobby/url.ts
@@ -61,8 +61,12 @@ export function parseAndGoto(data: ServerCommandWelcomeData): void {
     if (tableIDString !== undefined) {
       const tableID = parseIntSafe(tableIDString);
       if (tableID !== undefined && tableID > 0) {
+        // Here we unconditionally send password from cookies. If the game has no password, the
+        // server will ignore it.
+        const password = localStorage.getItem("joinTablePassword");
         globals.conn!.send("tableJoin", {
           tableID,
+          password,
         });
         return;
       }
