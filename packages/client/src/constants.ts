314c12da6bcd432cb5160414933297242afe8067
emusic99 <poem_wiser_6p@icloud.com>
2025-07-29 19:39:24 -0400

    Update constants.ts

--- packages/client/src/constants.ts
+++ packages/client/src/constants.ts
@@ -3,5 +3,5 @@
 /** In milliseconds. */
 export const FADE_TIME_MS = 350;
 
-export const SHUTDOWN_TIMEOUT_MINUTES = 30;
+export const SHUTDOWN_TIMEOUT_MINUTES = 60;
 export const TYPED_HISTORY_MAX_LENGTH = 250;
