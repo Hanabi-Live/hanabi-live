bec1c7f29c1c935fcb2f2117796fbdd908faa555
Max Cabrajac <max@cabrajac.com>
2025-10-22 01:32:15 -0300

    Revert comment formatting change This reverts commit 68ab5bdb1582606c78868a1cf2ba669a6e9f61af.

--- server/src/command_setting.go
+++ server/src/command_setting.go
@@ -10,12 +10,11 @@ import (
 
 // commandSetting is sent when the user modifies a setting
 //
-//	Example data:
-//
-//	{
-//	 name: 'soundMove',
-//	 setting: 'false', // All setting values must be strings
-//	}
+// 	Example data:
+// {
+//   name: 'soundMove',
+//   setting: 'false', // All setting values must be strings
+// }
 func commandSetting(ctx context.Context, s *Session, d *CommandData) {
 	// Validate the setting name
 	if d.Name == "" {
