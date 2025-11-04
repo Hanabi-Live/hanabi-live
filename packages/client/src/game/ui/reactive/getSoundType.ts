dcbe6f1de7aa7dfb2cf26f0071340b03372b6ee8
Tanmay Garg <40921153+tanmay2004@users.noreply.github.com>
2025-11-03 17:45:31 -0800

    Fix broken hanabi reference links

--- packages/client/src/game/ui/reactive/getSoundType.ts
+++ packages/client/src/game/ui/reactive/getSoundType.ts
@@ -212,10 +212,10 @@ export function getStandardSoundType(ourTurn: boolean): SoundType {
 }
 
 /**
- * https://hanabi.github.io/level_2/#playing-multiple-1s---play-order-inversion-in-the-starting-hand-part-1
- * https://hanabi.github.io/level_3/#playing-multiple-1s---the-fresh-1s-rule-part-2
- * https://hanabi.github.io/level_3/#playing-multiple-1s---the-chop-focus-exception-part-3
- * https://hanabi.github.io/level_5/#the-order-chop-move-ocm
+ * https://hanabi.github.io/level-3#part-1---play-order-inversion-in-the-starting-hand
+ * https://hanabi.github.io/level-3#part-2---the-fresh-1s-rule
+ * https://hanabi.github.io/level-3#part-3---the-chop-focus-exception
+ * https://hanabi.github.io/level-4/#the-order-chop-move-ocm
  */
 function isOrderChopMove(
   previousGameState: GameState,
