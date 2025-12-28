abb9edeb8fced81f289fa3406c8562fbea96e14b
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-12-28 17:54:48 -0500

    fix: lint

--- check_shellcheck.sh
+++ check_shellcheck.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail # Exit on errors and undefined variables.
 
+# Get the directory of this script:
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
 cd "$DIR"
 
 find . \( -name "node_modules" -o -name ".husky" \) -prune -o -type f -name "*.sh" -exec shellcheck {} +
